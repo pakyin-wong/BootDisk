6edde36f8173c112cd3e527e167fbea0b2707397
Pakyin Wong <pakyinwww@gmail.com>
2019-10-09 18:16:03 +0800

    Singleton modified

--- src/Director.ts
+++ src/Director.ts
@@ -1,5 +1,14 @@
 class Director {
+    private static _director: Director
+    private constructor() {
 
+    }
+
+    public static get Instance(): Director {
+        if (!this._director)
+            this._director = new Director()
+        return this._director
+    }
     socket: socket.MQTTSocketComm;
     evtHandler: handler.EventHandler;
     errHandler: handler.ErrorHandler;
@@ -9,13 +18,5 @@ class Director {
     // nav: 
 }
 
-if (!window.director) {
-    window.director = new Director();
-}
+let director: Director = Director.Instance;
 
-declare let director: Director;
-
-declare interface Window {
-
-    director: Director
-}
