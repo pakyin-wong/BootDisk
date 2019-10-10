b69a4dd906d0ccb199118785ecdffdbdd4f5f1bd
Simon Nam <simon.nam@coulla.com>
2019-10-10 16:19:50 +0800

    mq socket

--- src/comm/Socket.ts
+++ src/comm/Socket.ts
@@ -1,7 +1,22 @@
 module socket {
     export class MQTTSocketComm {
-        
+
         constructor(){
+            let client = new TestClient({
+                service: 'pg.srv.loadtest',
+                id: 'f5ef197c-6021-4f43-90b4-71749d217957',
+                secret: '4114f79f17c28ec6bfa01b80a28870a7',            
+                authhost: 'http://18.139.237.86:8080/loadtest/api/auth',
+                hostname:"18.139.237.86",
+                port:"15675", 
+                path:"/ws",
+                protocol:"ws"
+            })
+            client.subscribe("CONNECT",function(){
+                console.log("Connected")
+            })
+            client.connect()
+
             logger.l("MQTTSocketComm is created");
         }
     }
