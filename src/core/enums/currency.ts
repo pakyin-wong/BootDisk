7eafc9de7f5b474df74983f96d1174c5f53b464d
John Doe <johndoe@example.com>
2019-11-13 19:32:10 +0800

    add

---
+++ src/core/enums/currency.ts
@@ -0,0 +1,18 @@
+namespace we {
+  export namespace core {
+    export enum Currency {
+      EUR,
+      GBP,
+      HKD,
+      JPY,
+      KRW,
+      MYR,
+      RMB,
+      SGD,
+      THB,
+      TWD,
+      USD,
+      VND,
+    }
+  }
+}
