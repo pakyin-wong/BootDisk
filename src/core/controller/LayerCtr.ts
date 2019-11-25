02144eda6ca1f8d06c109b3ce56767da0f6d1851
Simon Nam <simon.nam@coulla.com>
2019-11-25 18:49:47 +0800

    nav update

--- src/core/controller/LayerCtr.ts
+++ src/core/controller/LayerCtr.ts
@@ -4,23 +4,23 @@ namespace we {
       public bottom: egret.Sprite;
       public scene: egret.Sprite;
       public betinfolist: egret.Sprite;
-      public nav: egret.Sprite;
       public top: egret.Sprite;
+      public nav: egret.Sprite;
       public overlay: egret.Sprite;
 
       constructor(stage: egret.Stage) {
         this.bottom = new egret.Sprite();
         this.scene = new egret.Sprite();
         this.betinfolist = new egret.Sprite();
-        this.nav = new egret.Sprite();
         this.top = new egret.Sprite();
+        this.nav = new egret.Sprite();
         this.overlay = new egret.Sprite();
 
         stage.addChild(this.bottom);
         stage.addChild(this.scene);
         stage.addChild(this.betinfolist);
-        stage.addChild(this.nav);
         stage.addChild(this.top);
+        stage.addChild(this.nav);
         stage.addChild(this.overlay);
 
         logger.l('LayerCtr is created');
