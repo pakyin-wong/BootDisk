36d815fd0cb418d9993378e901dfaf364bed7a4d
Pakyin Wong <pakyinwww@gmail.com>
2019-11-26 16:09:54 +0800

    Interim update

---
+++ src/game/live/components/LiveBaccaratExpanded.ts
@@ -0,0 +1,23 @@
+namespace we {
+  export namespace live {
+    export class LiveBacarratExpanded extends core.BaseEUI {
+      public constructor() {
+        super();
+      }
+      public childrenCreated(){
+        super.childrenCreated();
+        
+      }
+      public setHeight(){
+
+      }
+      public setWidth(){
+
+      }
+      public setTableId(){
+
+      }
+
+    }
+  }
+}
