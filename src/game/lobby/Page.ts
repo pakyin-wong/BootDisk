3c68a1cb1cbf5d827bb506d05b2f183824641ba9
John Doe <johndoe@example.com>
2019-12-03 20:21:01 +0800

    updated lobby interface

--- src/game/lobby/Page.ts
+++ src/game/lobby/Page.ts
@@ -27,39 +27,75 @@ namespace we {
         const offsetForTableList = -paddingHorizontal * 3;
         const gapSize = 48;
 
-        // init image slider and category tab
+        // init image slider
         const slider = new we.ui.ImageSlider();
         slider.width = this.scroller.width;
         slider.height = 790;
         group.addChild(slider);
 
-        // init sections
-        const sections = new eui.Group();
-        const vlayout = new eui.VerticalLayout();
-        vlayout.paddingLeft = paddingHorizontal;
-        vlayout.paddingRight = paddingHorizontal;
-        sections.layout = vlayout;
-        sections.y = slider.height + offsetForTableList;
-        const createSection = (title, items) => {
-          const label = new eui.Label();
-          label.size = 40;
-          label.height = 100;
-          label.verticalAlign = egret.VerticalAlign.MIDDLE;
-          label.text = i18n.t(`lobby.header.${title}`);
-          const tcslider = new we.lobby.ThreeColumnSlider();
-          tcslider.width = this.scroller.width - paddingHorizontal * 2;
-          tcslider.height = 500;
-          tcslider.items = items;
-          sections.addChild(label);
-          sections.addChild(tcslider);
-        };
+        // init 4 featured posters
+        const posters = new eui.Group();
+        const hlayout = new eui.HorizontalLayout();
+        hlayout.horizontalAlign = egret.HorizontalAlign.JUSTIFY;
+        hlayout.gap = gapSize;
+        posters.layout = hlayout;
+        posters.left = paddingHorizontal;
+        posters.right = paddingHorizontal;
+        posters.y = slider.height + offsetForTableList;
+        for (let i = 1; i <= 4; i += 1) {
+          const image = new eui.Image();
+          image.source = RES.getRes(`lobby-featured-poster-${i}_png`);
+          image.height = 800;
+          posters.addChild(image);
+        }
+        group.addChild(posters);
 
-        createSection('lobby', ['h4_png', 'h5_png']);
-        createSection('live', ['h4_png', 'h5_png', 'h6_png']);
-        createSection('lottery', ['h4_png', 'h5_png', 'h6_png', 'h7_png']);
-        createSection('egame', ['h4_png', 'h5_png', 'h6_png', 'h7_png', 'h8_png']);
-        createSection('favorite', ['h4_png', 'h5_png', 'h6_png', 'h7_png', 'h8_png', 'h9_png', 'h10_png']);
-        group.addChild(sections);
+        // init 3 grids
+        const grids = new eui.Group();
+        const tlayout = new eui.TileLayout();
+        tlayout.requestedColumnCount = 3;
+        tlayout.horizontalGap = gapSize;
+        tlayout.verticalGap = gapSize;
+        tlayout.columnWidth = (2600 - paddingHorizontal * 2 - paddingHorizontal * (tlayout.requestedColumnCount - 1)) / tlayout.requestedColumnCount;
+        grids.layout = tlayout;
+        grids.left = paddingHorizontal;
+        grids.right = paddingHorizontal;
+        grids.y = posters.y + gapSize + 800;
+        const images = [RES.getRes('temp_baccarat_dealer_1'), RES.getRes('temp_baccarat_dealer_1'), RES.getRes('temp_baccarat_dealer_1'), RES.getRes('temp_baccarat_dealer_1')];
+        images.forEach(res => {
+          const image = new eui.Image();
+          image.source = res;
+          grids.addChild(image);
+        });
+        group.addChild(grids);
+
+        // init sections
+        // const sections = new eui.Group();
+        // const vlayout = new eui.VerticalLayout();
+        // vlayout.paddingLeft = paddingHorizontal;
+        // vlayout.paddingRight = paddingHorizontal;
+        // sections.layout = vlayout;
+        // sections.y = slider.height + offsetForTableList;
+        // const createSection = (title, items) => {
+        //   const label = new eui.Label();
+        //   label.size = 40;
+        //   label.height = 100;
+        //   label.verticalAlign = egret.VerticalAlign.MIDDLE;
+        //   label.text = i18n.t(`lobby.header.${title}`);
+        //   const tcslider = new we.lobby.ThreeColumnSlider();
+        //   tcslider.width = this.scroller.width - paddingHorizontal * 2;
+        //   tcslider.height = 500;
+        //   tcslider.items = items;
+        //   sections.addChild(label);
+        //   sections.addChild(tcslider);
+        // };
+
+        // createSection('lobby', ['h4_png', 'h5_png']);
+        // createSection('live', ['h4_png', 'h5_png', 'h6_png']);
+        // createSection('lottery', ['h4_png', 'h5_png', 'h6_png', 'h7_png']);
+        // createSection('egame', ['h4_png', 'h5_png', 'h6_png', 'h7_png', 'h8_png']);
+        // createSection('favorite', ['h4_png', 'h5_png', 'h6_png', 'h7_png', 'h8_png', 'h9_png', 'h10_png']);
+        // group.addChild(sections);
 
         // init three banner
         // const bannerList = new ui.List();
