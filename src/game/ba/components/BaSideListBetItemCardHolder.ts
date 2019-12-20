8234bf0aeb7f2f9f27b3c02245f0cc690be23ca4
Pakyin Wong <pakyinwww@gmail.com>
2019-12-20 13:24:22 +0800

    Update Ba Side List Bet ITem Card Holder

--- src/game/ba/components/BaSideListBetItemCardHolder.ts
+++ src/game/ba/components/BaSideListBetItemCardHolder.ts
@@ -1,18 +1,56 @@
 namespace we {
   export namespace ba {
     export class BaSideListBetItemCardHolder extends CardHolder {
-      constructor() {
-        super();
-      }
+      protected _playerPanel: eui.Image;
+      protected _bankerPanel: eui.Image;
 
       protected createChildren() {
         super.createChildren();
         this.skinName = utils.getSkin('BaSideListBetItemCardHolder');
       }
 
-      public setWinningSide() {}
+      constructor() {
+        super();
+      }
 
-      public resetWinningSide() {}
+      public updateResult(gameData: GameData) {
+        super.updateResult(gameData);
+        switch (gameData.wintype) {
+          case we.ba.WinType.PLAYER:
+            this.setPlayerBgColor(true);
+            this.setBankerBgColor(false);
+            break;
+          case we.ba.WinType.BANKER:
+            this.setPlayerBgColor(false);
+            this.setBankerBgColor(true);
+            break;
+          case we.ba.WinType.TIE:
+            this.setPlayerBgColor(false);
+            this.setBankerBgColor(false);
+            break;
+          case we.ba.WinType.NONE:
+            this.setPlayerBgColor(false);
+            this.setBankerBgColor(false);
+          default:
+            break;
+        }
+      }
+
+      public setPlayerBgColor(value: boolean) {
+        if (value) {
+          this._playerPanel.source = 'd_lobby_panel_gamelist_betresult_player_png';
+        } else {
+          this._playerPanel.source = 'd_lobby_panel_gamelist_betresult_playbg_png';
+        }
+      }
+
+      public setBankerBgColor(value: boolean) {
+        if (value) {
+          this._bankerPanel.source = 'd_lobby_panel_gamelist_betresult_banker_png';
+        } else {
+          this._bankerPanel.source = 'd_lobby_panel_gamelist_betresult_bankerbg_png';
+        }
+      }
     }
   }
 }
