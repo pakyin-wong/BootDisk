9898c3cdd08759261b326b4942fa6e4eb42de862
Joe Ng <joe.dev@pgpg.la>
2020-01-13 13:12:26 +0800

    - Updated dragonbone animation test

--- src/game/lobby/Page.ts
+++ src/game/lobby/Page.ts
@@ -133,89 +133,89 @@ namespace we {
 
         this.scroller.viewport = group;
 
-        // Dragonbone animation
-        const skeletonData = RES.getRes('game_result_test_ske_json');
-        const textureData = RES.getRes('game_result_test_tex_json');
-        const texture = RES.getRes('game_result_test_tex_png');
-        const factory = new dragonBones.EgretFactory();
-        factory.parseDragonBonesData(skeletonData);
-        factory.parseTextureAtlasData(textureData, texture);
-        logger.l('1>', factory.getAllDragonBonesData(), factory.getAllTextureAtlasData());
-        const armatureDisplay = factory.buildArmatureDisplay('Armature');
-        armatureDisplay.x = this.$stage.stageWidth / 2;
-        armatureDisplay.y = this.$stage.stageHeight / 2;
-        this.addChild(armatureDisplay);
-        armatureDisplay.animation.play('win_start', -1);
+        // // Dragonbone animation
+        // const skeletonData = RES.getRes('game_result_test_ske_json');
+        // const textureData = RES.getRes('game_result_test_tex_json');
+        // const texture = RES.getRes('game_result_test_tex_png');
+        // const factory = new dragonBones.EgretFactory();
+        // factory.parseDragonBonesData(skeletonData);
+        // factory.parseTextureAtlasData(textureData, texture);
+        // logger.l('1>', factory.getAllDragonBonesData(), factory.getAllTextureAtlasData());
+        // const armatureDisplay = factory.buildArmatureDisplay('Armature');
+        // armatureDisplay.x = this.$stage.stageWidth / 2;
+        // armatureDisplay.y = this.$stage.stageHeight / 2;
+        // this.addChild(armatureDisplay);
+        // armatureDisplay.animation.play('win_start', -1);
 
-        const slot = armatureDisplay.armature.getSlot('+800');
-        const r = new eui.Rect();
-        r.height = 50;
-        r.width = 50;
-        r.fillColor = 0xff0000;
-        slot.display = r;
+        // const slot = armatureDisplay.armature.getSlot('+800');
+        // const r = new eui.Rect();
+        // r.height = 50;
+        // r.width = 50;
+        // r.fillColor = 0xff0000;
+        // slot.display = r;
 
-        // Dragonbone animation 2 (self gen sprite)
-        const skeletonData2 = RES.getRes('Icon_ske_json');
-        const factory2 = new dragonBones.EgretFactory();
-        factory2.parseDragonBonesData(skeletonData2);
-        const atlas = new dragonBones.EgretTextureAtlasData();
+        // // Dragonbone animation 2 (self gen sprite)
+        // const skeletonData2 = RES.getRes('Icon_ske_json');
+        // const factory2 = new dragonBones.EgretFactory();
+        // factory2.parseDragonBonesData(skeletonData2);
+        // const atlas = new dragonBones.EgretTextureAtlasData();
 
-        const resNames = ['Sound_Off_Line_png', 'Sound_On_Line_png', 'Sound_On_png'];
-        const res = resNames.map(RES.getRes);
-        const mergedTex = this.combineToSprite(res);
-        let offsetX = 0;
+        // const resNames = ['Sound_Off_Line_png', 'Sound_On_Line_png', 'Sound_On_png'];
+        // const res = resNames.map(RES.getRes);
+        // const mergedTex = this.combineToSprite(res);
+        // let offsetX = 0;
 
-        for (let i = 0; i < resNames.length; i += 1) {
-          const tex1 = new dragonBones.EgretTextureData();
-          tex1.rotated = mergedTex.$rotated;
-          tex1.name = resNames[i].replace('_png', '');
-          tex1.region.x = offsetX;
-          tex1.region.y = 0;
-          tex1.region.width = res[i].textureWidth;
-          tex1.region.height = res[i].textureHeight;
-          tex1.frame = null;
-          tex1.renderTexture = mergedTex;
-          tex1.parent = atlas;
-          atlas.addTexture(tex1);
-          offsetX += res[i].textureWidth;
-        }
-
-        atlas.renderTexture = mergedTex;
-
-        // function cleanStringify(object) {
-        //   if (object && typeof object === 'object') {
-        //     object = copyWithoutCircularReferences([object], object);
-        //   }
-        //   return JSON.stringify(object);
-
-        //   function copyWithoutCircularReferences(references, object) {
-        //     const cleanObject = {};
-        //     Object.keys(object).forEach(function (key) {
-        //       const value = object[key];
-        //       if (value && typeof value === 'object') {
-        //         if (references.indexOf(value) < 0) {
-        //           references.push(value);
-        //           cleanObject[key] = copyWithoutCircularReferences(references, value);
-        //           references.pop();
-        //         } else {
-        //           cleanObject[key] = '###_Circular_###';
-        //         }
-        //       } else if (typeof value !== 'function') {
-        //         cleanObject[key] = value;
-        //       }
-        //     });
-        //     return cleanObject;
-        //   }
+        // for (let i = 0; i < resNames.length; i += 1) {
+        //   const tex1 = new dragonBones.EgretTextureData();
+        //   tex1.rotated = mergedTex.$rotated;
+        //   tex1.name = resNames[i].replace('_png', '');
+        //   tex1.region.x = offsetX;
+        //   tex1.region.y = 0;
+        //   tex1.region.width = res[i].textureWidth;
+        //   tex1.region.height = res[i].textureHeight;
+        //   tex1.frame = null;
+        //   tex1.renderTexture = mergedTex;
+        //   tex1.parent = atlas;
+        //   atlas.addTexture(tex1);
+        //   offsetX += res[i].textureWidth;
         // }
 
-        factory2.addTextureAtlasData(atlas, 'Icon');
-        logger.l('2>', factory2.getAllDragonBonesData(), factory2.getAllTextureAtlasData());
-        // factory.parseTextureAtlasData(textureData, texture);
-        const armatureDisplay2 = factory2.buildArmatureDisplay('Sound');
-        armatureDisplay2.x = 160;
-        armatureDisplay2.y = 160;
-        this.addChild(armatureDisplay2);
-        armatureDisplay2.animation.play('Sound_close_ani', 0);
+        // atlas.renderTexture = mergedTex;
+
+        // // function cleanStringify(object) {
+        // //   if (object && typeof object === 'object') {
+        // //     object = copyWithoutCircularReferences([object], object);
+        // //   }
+        // //   return JSON.stringify(object);
+
+        // //   function copyWithoutCircularReferences(references, object) {
+        // //     const cleanObject = {};
+        // //     Object.keys(object).forEach(function (key) {
+        // //       const value = object[key];
+        // //       if (value && typeof value === 'object') {
+        // //         if (references.indexOf(value) < 0) {
+        // //           references.push(value);
+        // //           cleanObject[key] = copyWithoutCircularReferences(references, value);
+        // //           references.pop();
+        // //         } else {
+        // //           cleanObject[key] = '###_Circular_###';
+        // //         }
+        // //       } else if (typeof value !== 'function') {
+        // //         cleanObject[key] = value;
+        // //       }
+        // //     });
+        // //     return cleanObject;
+        // //   }
+        // // }
+
+        // factory2.addTextureAtlasData(atlas, 'Icon');
+        // logger.l('2>', factory2.getAllDragonBonesData(), factory2.getAllTextureAtlasData());
+        // // factory.parseTextureAtlasData(textureData, texture);
+        // const armatureDisplay2 = factory2.buildArmatureDisplay('Sound');
+        // armatureDisplay2.x = 160;
+        // armatureDisplay2.y = 160;
+        // this.addChild(armatureDisplay2);
+        // armatureDisplay2.animation.play('Sound_close_ani', 0);
 
         // Dragonbone animation 3 (sprite)
         const skeletonData3 = RES.getRes('Icon2_ske_json');
@@ -230,39 +230,49 @@ namespace we {
         armatureDisplay3.y = 160;
         this.addChild(armatureDisplay3);
         armatureDisplay3.animation.play('Sound_close_ani', 0);
+        const armatureDisplay2 = factory3.buildArmatureDisplay('Option');
+        armatureDisplay2.x = 160;
+        armatureDisplay2.y = 160;
+        this.addChild(armatureDisplay2);
+        armatureDisplay2.animation.play('Option_close_ani', 0);
+        const armatureDisplay1 = factory3.buildArmatureDisplay('Info');
+        armatureDisplay1.x = 280;
+        armatureDisplay1.y = 160;
+        this.addChild(armatureDisplay1);
+        armatureDisplay1.animation.play('Sound_close_ani', 0);
 
-        // Dragonbone animation 4 (non sprite)
-        const skeletonData4 = RES.getRes('Icon_ske_json');
-        const factory4 = new dragonBones.EgretFactory();
-        factory4.parseDragonBonesData(skeletonData4);
-        const atlas4 = new dragonBones.EgretTextureAtlasData();
+        // // Dragonbone animation 4 (non sprite)
+        // const skeletonData4 = RES.getRes('Icon_ske_json');
+        // const factory4 = new dragonBones.EgretFactory();
+        // factory4.parseDragonBonesData(skeletonData4);
+        // const atlas4 = new dragonBones.EgretTextureAtlasData();
 
-        for (const name of ['Sound_Off_Line_png', 'Sound_On_Line_png', 'Sound_On_png']) {
-          const rawTex1 = RES.getRes(name) as egret.Texture;
-          const tex1 = atlas.createTexture() as dragonBones.EgretTextureData;
-          tex1.rotated = rawTex1.$rotated;
-          tex1.name = name.replace('_png', '');
-          tex1.region.x = 0.0;
-          tex1.region.y = 0.0;
-          tex1.region.width = rawTex1.textureWidth;
-          tex1.region.height = rawTex1.textureHeight;
-          tex1.frame = dragonBones.TextureData.createRectangle();
-          tex1.frame.x = 0.0;
-          tex1.frame.y = 0.0;
-          tex1.frame.width = rawTex1.textureWidth;
-          tex1.frame.height = rawTex1.textureHeight;
-          tex1.renderTexture = rawTex1;
-          atlas4.addTexture(tex1);
-        }
+        // for (const name of ['Sound_Off_Line_png', 'Sound_On_Line_png', 'Sound_On_png']) {
+        //   const rawTex1 = RES.getRes(name) as egret.Texture;
+        //   const tex1 = atlas.createTexture() as dragonBones.EgretTextureData;
+        //   tex1.rotated = rawTex1.$rotated;
+        //   tex1.name = name.replace('_png', '');
+        //   tex1.region.x = 0.0;
+        //   tex1.region.y = 0.0;
+        //   tex1.region.width = rawTex1.textureWidth;
+        //   tex1.region.height = rawTex1.textureHeight;
+        //   tex1.frame = dragonBones.TextureData.createRectangle();
+        //   tex1.frame.x = 0.0;
+        //   tex1.frame.y = 0.0;
+        //   tex1.frame.width = rawTex1.textureWidth;
+        //   tex1.frame.height = rawTex1.textureHeight;
+        //   tex1.renderTexture = rawTex1;
+        //   atlas4.addTexture(tex1);
+        // }
 
-        factory4.addTextureAtlasData(atlas4, 'Icon');
-        logger.l('4>', factory4.getAllDragonBonesData(), factory4.getAllTextureAtlasData());
-        // factory.parseTextureAtlasData(textureData, texture);
-        const armatureDisplay4 = factory4.buildArmatureDisplay('Sound');
-        armatureDisplay4.x = 300;
-        armatureDisplay4.y = 160;
-        this.addChild(armatureDisplay4);
-        armatureDisplay4.animation.play('Sound_open_ani', 0);
+        // factory4.addTextureAtlasData(atlas4, 'Icon');
+        // logger.l('4>', factory4.getAllDragonBonesData(), factory4.getAllTextureAtlasData());
+        // // factory.parseTextureAtlasData(textureData, texture);
+        // const armatureDisplay4 = factory4.buildArmatureDisplay('Sound');
+        // armatureDisplay4.x = 300;
+        // armatureDisplay4.y = 160;
+        // this.addChild(armatureDisplay4);
+        // armatureDisplay4.animation.play('Sound_open_ani', 0);
 
         // const blendShape = new egret.Shape();
         // blendShape.graphics.beginGradientFill(egret.GradientType.LINEAR, [0xffffff, 0xffffff], [1, 0], [0, 255]);
