01a54c685158865e124ebb5d615736e8075be412
Joe Ng <joe.dev@pgpg.la>
2020-02-11 19:01:11 +0800

    - Updated OrientationManager

--- src/utils/OrientationManager.ts
+++ src/utils/OrientationManager.ts
@@ -7,19 +7,20 @@ namespace we {
       constructor(stage: egret.Stage) {
         this.stage = stage;
         window.onorientationchange = () => {
-          this.onRotate((<any> screen).orientation.angle);
+          this.onRotate((<any>screen).orientation.angle);
         };
+        this.onRotate((<any>screen).orientation.angle);
       }
 
       public onRotate(angle: number) {
         if (angle === 0) {
           // portrait
           console.log('portrait');
-          // this.stage.setContentSize(1340, 2600);
+          this.stage.setContentSize(1242, 2292);
         } else {
           // landscape
           console.log('landscape');
-          // this.stage.setContentSize(2600, 1340);
+          this.stage.setContentSize(2292, 1242);
         }
       }
     }
