bc6b5178e61cf3cb819b7a05015c4d603dd7872d
Joe Ng <joe.dev@pgpg.la>
2020-02-13 17:48:52 +0800

    - Updated MPageContentInitializer to support both portrait and landscape

--- src/game/lobby/MPageContentInitializer.ts
+++ src/game/lobby/MPageContentInitializer.ts
@@ -5,6 +5,14 @@ namespace we {
       constructor() {}
 
       public initContent(root: Page) {
+        if (env.orientation === egret.OrientationMode.PORTRAIT) {
+          this.initPortraitContent(root);
+        } else {
+          this.initLandscapeContent(root);
+        }
+      }
+
+      public initPortraitContent(root: Page) {
         const group = new eui.Group();
         let vlayout = new eui.VerticalLayout();
         vlayout.gap = 0;
@@ -131,36 +139,10 @@ namespace we {
         footer.addChild(label);
         group.addChild(footer);
 
-        // init sections
-        // const sections = new eui.Group();
-        // const vlayout = new eui.VerticalLayout();
-        // vlayout.paddingLeft = paddingHorizontal;
-        // vlayout.paddingRight = paddingHorizontal;
-        // sections.layout = vlayout;
-        // sections.y = slider.height + offsetForTableList;
-        // const createSection = (title, items) => {
-        //   const label = new eui.Label();
-        //   label.size = 40;
-        //   label.height = 100;
-        //   label.verticalAlign = egret.VerticalAlign.MIDDLE;
-        //   label.text = i18n.t(`lobby.header.${title}`);
-        //   const tcslider = new we.lobby.ThreeColumnSlider();
-        //   tcslider.width = this.scroller.width - paddingHorizontal * 2;
-        //   tcslider.height = 500;
-        //   tcslider.items = items;
-        //   sections.addChild(label);
-        //   sections.addChild(tcslider);
-        // };
-
-        // createSection('lobby', ['h4_png', 'h5_png']);
-        // createSection('live', ['h4_png', 'h5_png', 'h6_png']);
-        // createSection('lottery', ['h4_png', 'h5_png', 'h6_png', 'h7_png']);
-        // createSection('egame', ['h4_png', 'h5_png', 'h6_png', 'h7_png', 'h8_png']);
-        // createSection('favorite', ['h4_png', 'h5_png', 'h6_png', 'h7_png', 'h8_png', 'h9_png', 'h10_png']);
-        // group.addChild(sections);
-
         root.scroller.viewport = group;
       }
+
+      public initLandscapeContent(root: Page) {}
     }
   }
 }
