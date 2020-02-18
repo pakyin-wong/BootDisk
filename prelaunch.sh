01e36567bdb5fb8578af64fc814f46122efe5761
pgsteven <s@iMac151.local>
2020-02-18 19:24:13 +0800

    add animations & update prelaunch.sh

--- prelaunch.sh
+++ prelaunch.sh
@@ -21,12 +21,20 @@ if [ -f /tmp/egretlastrun.tmp ]; then
 fi
 lastrun=$(expr $now - $lastrun)
 
-echo $now > /tmp/egretlastrun.tmp
+# echo $now > /tmp/egretlastrun.tmp
 
-if test $lastrun -gt 60; then
+if test $lastrun -gt 1; then
     # greater than 30 seconds
     echo "[prelaunch] started linting (${lastrun}s)"
-    sleep 1 && prettier --write 'src/**/*.ts' && sleep 0.5 && tslint -c tslint.json --fix 'src/**/*.ts' && sleep 0.5 && $bin $@
+    # $0[0]=space=unstaged, $0[1]=character=staged (git added)
+    flist="$(git status --porcelain | awk '{ if ( (substr($0,1,1) ~ /^[[:space:]]$/ || substr($0,1,1) !~ /^[[:space:]]$/) && $2 ~ /\.ts$/ ) print $2 }')"
+    flish="$(echo $flist | tr '\r\n' ' ')"
+    if [ -z "$var" ]; then
+        sleep 1 && prettier --write $flist && sleep 0.5 && tslint -c tslint.json --fix $flist && sleep 0.5 && $bin $@
+        # sleep 1 && prettier --write 'src/**/*.ts' && sleep 0.5 && tslint -c tslint.json --fix 'src/**/*.ts' && sleep 0.5 && $bin $@
+    else
+        sleep 1 && $bin $@
+    fi
 else
     # skip linting
     echo "[prelaunch] skipped linting for this run (${lastrun}s)"
