925d266e5e0ba6c88c191226deaba47ff44cd5d3
pgsteven <s@iMac151.local>
2020-02-18 19:43:48 +0800

    add animations & update prelaunch.sh

--- prelaunch.sh
+++ prelaunch.sh
@@ -21,19 +21,19 @@ if [ -f /tmp/egretlastrun.tmp ]; then
 fi
 lastrun=$(expr $now - $lastrun)
 
-# echo $now > /tmp/egretlastrun.tmp
+echo $now > /tmp/egretlastrun.tmp
 
-if test $lastrun -gt 1; then
+if test $lastrun -gt 60; then
     # greater than 30 seconds
     echo "[prelaunch] started linting (${lastrun}s)"
     # $0[0]=space=unstaged, $0[1]=character=staged (git added)
     flist="$(git status --porcelain | awk '{ if ( (substr($0,1,1) ~ /^[[:space:]]$/ || substr($0,1,1) !~ /^[[:space:]]$/) && $2 ~ /\.ts$/ ) print $2 }')"
-    flish="$(echo $flist | tr '\r\n' ' ')"
-    if [ -z "$var" ]; then
-        sleep 1 && prettier --write $flist && sleep 0.5 && tslint -c tslint.json --fix $flist && sleep 0.5 && $bin $@
-        # sleep 1 && prettier --write 'src/**/*.ts' && sleep 0.5 && tslint -c tslint.json --fix 'src/**/*.ts' && sleep 0.5 && $bin $@
+    echo -e "[prelaunch] file list:\n$flist"
+    flist="$(echo $flist | tr '\r\n' ' ' | awk '{$1=$1};1')"
+    if [ ! "$flist" ]; then
+        sleep 1 && prettier --write 'src/**/*.ts' && sleep 0.5 && tslint -c tslint.json --fix 'src/**/*.ts' && sleep 0.5 && $bin $@
     else
-        sleep 1 && $bin $@
+        sleep 1 && prettier --write $flist && sleep 0.5 && tslint -c tslint.json --fix $flist && sleep 0.5 && $bin $@
     fi
 else
     # skip linting
