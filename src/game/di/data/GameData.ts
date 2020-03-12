742393b87cba528f6c86868b44c308609b206eb8
Pakyin Wong <pakyinwww@gmail.com>
2020-03-12 12:11:02 +0800

    Update Dice GameData

--- src/game/di/data/GameData.ts
+++ src/game/di/data/GameData.ts
@@ -1,7 +1,13 @@
 namespace we {
   export namespace di {
     export class GameData extends data.GameData {
-      public value: number;
+      public dice1: number        // Value of the dice 1
+      public dice2: number        // Value of the dice 2  
+      public dice3: number        // Value of the dice 3  
+      public total: number        // Total dice1+dice2+dice3
+      public odd: number          // Odd/Even=1/2
+      public size: number         // Small/Big=1/2
+      public tie: number          // dice1=dice2=dice3?1:0
     }
   }
 }
--- src/game/ro/enums/GameSetting.ts
+++ src/game/ro/enums/GameSetting.ts
@@ -21,20 +21,6 @@ namespace we {
         ro.BetField.SEPARATE_23_24,
         ro.BetField.SEPARATE_27_30,
         ro.BetField.SEPARATE_31_34,
-        /*
-        ro.BetField.DIRECT_27,
-        ro.BetField.DIRECT_13,
-        ro.BetField.DIRECT_36,
-        ro.BetField.DIRECT_11,
-        ro.BetField.DIRECT_30,
-        ro.BetField.DIRECT_8,
-        ro.BetField.DIRECT_23,
-        ro.BetField.DIRECT_10,
-        ro.BetField.DIRECT_5,
-        ro.BetField.DIRECT_24,
-        ro.BetField.DIRECT_16,
-        ro.BetField.DIRECT_33,
-        */
       ],
       [ro.RACETRACK_INNERFIELD.ORPHELINS]: [
         ro.BetField.SEPARATE_6_9,
@@ -42,15 +28,6 @@ namespace we {
         ro.BetField.SEPARATE_17_20,
         ro.BetField.SEPARATE_31_34,
         ro.BetField.DIRECT_1,
-        /*
-        ro.BetField.DIRECT_20,
-        ro.BetField.DIRECT_14,
-        ro.BetField.DIRECT_31,
-        ro.BetField.DIRECT_9,
-        ro.BetField.DIRECT_17,
-        ro.BetField.DIRECT_34,
-        ro.BetField.DIRECT_6,
-        */
       ],
       [ro.RACETRACK_INNERFIELD.VOISINS]: [
         ro.BetField.STREET_0_2_3,
@@ -60,18 +37,6 @@ namespace we {
         ro.BetField.SEPARATE_19_22,
         ro.BetField.CORNER_25_26_28_29,
         ro.BetField.SEPARATE_32_35,
-        /*
-        ro.BetField.DIRECT_22,
-        ro.BetField.DIRECT_18,
-        ro.BetField.DIRECT_29,
-        ro.BetField.DIRECT_7,
-        ro.BetField.DIRECT_28,
-        ro.BetField.DIRECT_19,
-        ro.BetField.DIRECT_4,
-        ro.BetField.DIRECT_21,
-        ro.BetField.DIRECT_2,
-        ro.BetField.DIRECT_25,
-        */
       ],
       [ro.RACETRACK_INNERFIELD.ZERO]: [
         ro.BetField.SEPARATE_0_3,
@@ -79,15 +44,6 @@ namespace we {
         ro.BetField.SEPARATE_18_21,
         ro.BetField.DIRECT_26,
         ro.BetField.SEPARATE_32_35,
-        /*
-        ro.BetField.DIRECT_12,
-        ro.BetField.DIRECT_35,
-        ro.BetField.DIRECT_3,
-        ro.BetField.DIRECT_26,
-        ro.BetField.DIRECT_0,
-        ro.BetField.DIRECT_32,
-        ro.BetField.DIRECT_15
-        */
       ],
     };
 
