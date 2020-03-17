42be719d018d0afc06644313a3e43260081578b4
pakyin-wong <pakyin.wong@link.cuhk.edu.hk>
2020-03-18 01:06:45 +0800

    Update Bet Combination

---
+++ src/data/BetCombination.ts
@@ -0,0 +1,11 @@
+namespace we {
+  export namespace data {
+    export class BetCombination {
+      public title: string;
+      public playerid: string;
+      public option: we.data.BetValueOption[];
+      public id: string;
+      public gameType: string;
+    }
+  }
+}
