b8a3a40177e74b4c3f91fb3f1661c83f99f3b5de
Joe Ng <joe.dev@pgpg.la>
2020-03-18 17:39:19 +0800

    - updated mobile ro RaceTrackChipLayer class (draft)

--- src/game/ro/components/mobile/RaceTrackChipLayer.draft.ts
+++ src/game/ro/components/mobile/RaceTrackChipLayer.draft.ts
@@ -85,9 +85,6 @@ namespace we {
       protected _section_voisins_betChipStack: ui.BetChipStack;
       protected _section_zero_betChipStack: ui.BetChipStack;
 
-      protected _numberToSectionMapping: { [a: number]: string };
-      protected _sectionToNumberMapping: { [s: string]: number };
-
       protected _raceTrackControl: RaceTrackControl;
       protected _raceTrackTableLayer: RaceTrackTableLayer;
 
@@ -142,84 +139,6 @@ namespace we {
         this._mouseAreaMapping[ro.BetField.NEIGHBORS_OF_ZERO] = this._section_voisins;
         this._mouseAreaMapping[ro.BetField.ZERO_GAME] = this._section_zero;
 
-        this._sectionToNumberMapping = {};
-        this._sectionToNumberMapping[we.ro.BetField.DIRECT_0] = 0;
-        this._sectionToNumberMapping[we.ro.BetField.DIRECT_1] = 1;
-        this._sectionToNumberMapping[we.ro.BetField.DIRECT_2] = 2;
-        this._sectionToNumberMapping[we.ro.BetField.DIRECT_3] = 3;
-        this._sectionToNumberMapping[we.ro.BetField.DIRECT_4] = 4;
-        this._sectionToNumberMapping[we.ro.BetField.DIRECT_5] = 5;
-        this._sectionToNumberMapping[we.ro.BetField.DIRECT_6] = 6;
-        this._sectionToNumberMapping[we.ro.BetField.DIRECT_7] = 7;
-        this._sectionToNumberMapping[we.ro.BetField.DIRECT_8] = 8;
-        this._sectionToNumberMapping[we.ro.BetField.DIRECT_9] = 9;
-        this._sectionToNumberMapping[we.ro.BetField.DIRECT_10] = 10;
-        this._sectionToNumberMapping[we.ro.BetField.DIRECT_11] = 11;
-        this._sectionToNumberMapping[we.ro.BetField.DIRECT_12] = 12;
-        this._sectionToNumberMapping[we.ro.BetField.DIRECT_13] = 13;
-        this._sectionToNumberMapping[we.ro.BetField.DIRECT_14] = 14;
-        this._sectionToNumberMapping[we.ro.BetField.DIRECT_15] = 15;
-        this._sectionToNumberMapping[we.ro.BetField.DIRECT_16] = 16;
-        this._sectionToNumberMapping[we.ro.BetField.DIRECT_17] = 17;
-        this._sectionToNumberMapping[we.ro.BetField.DIRECT_18] = 18;
-        this._sectionToNumberMapping[we.ro.BetField.DIRECT_19] = 19;
-        this._sectionToNumberMapping[we.ro.BetField.DIRECT_20] = 20;
-        this._sectionToNumberMapping[we.ro.BetField.DIRECT_21] = 21;
-        this._sectionToNumberMapping[we.ro.BetField.DIRECT_22] = 22;
-        this._sectionToNumberMapping[we.ro.BetField.DIRECT_23] = 23;
-        this._sectionToNumberMapping[we.ro.BetField.DIRECT_24] = 24;
-        this._sectionToNumberMapping[we.ro.BetField.DIRECT_25] = 25;
-        this._sectionToNumberMapping[we.ro.BetField.DIRECT_26] = 26;
-        this._sectionToNumberMapping[we.ro.BetField.DIRECT_27] = 27;
-        this._sectionToNumberMapping[we.ro.BetField.DIRECT_28] = 28;
-        this._sectionToNumberMapping[we.ro.BetField.DIRECT_29] = 29;
-        this._sectionToNumberMapping[we.ro.BetField.DIRECT_30] = 30;
-        this._sectionToNumberMapping[we.ro.BetField.DIRECT_31] = 31;
-        this._sectionToNumberMapping[we.ro.BetField.DIRECT_32] = 32;
-        this._sectionToNumberMapping[we.ro.BetField.DIRECT_33] = 33;
-        this._sectionToNumberMapping[we.ro.BetField.DIRECT_34] = 34;
-        this._sectionToNumberMapping[we.ro.BetField.DIRECT_35] = 35;
-        this._sectionToNumberMapping[we.ro.BetField.DIRECT_36] = 36;
-
-        this._numberToSectionMapping = {};
-        this._numberToSectionMapping[0] = we.ro.BetField.DIRECT_0;
-        this._numberToSectionMapping[1] = we.ro.BetField.DIRECT_1;
-        this._numberToSectionMapping[2] = we.ro.BetField.DIRECT_2;
-        this._numberToSectionMapping[3] = we.ro.BetField.DIRECT_3;
-        this._numberToSectionMapping[4] = we.ro.BetField.DIRECT_4;
-        this._numberToSectionMapping[5] = we.ro.BetField.DIRECT_5;
-        this._numberToSectionMapping[6] = we.ro.BetField.DIRECT_6;
-        this._numberToSectionMapping[7] = we.ro.BetField.DIRECT_7;
-        this._numberToSectionMapping[8] = we.ro.BetField.DIRECT_8;
-        this._numberToSectionMapping[9] = we.ro.BetField.DIRECT_9;
-        this._numberToSectionMapping[10] = we.ro.BetField.DIRECT_10;
-        this._numberToSectionMapping[11] = we.ro.BetField.DIRECT_11;
-        this._numberToSectionMapping[12] = we.ro.BetField.DIRECT_12;
-        this._numberToSectionMapping[13] = we.ro.BetField.DIRECT_13;
-        this._numberToSectionMapping[14] = we.ro.BetField.DIRECT_14;
-        this._numberToSectionMapping[15] = we.ro.BetField.DIRECT_15;
-        this._numberToSectionMapping[16] = we.ro.BetField.DIRECT_16;
-        this._numberToSectionMapping[17] = we.ro.BetField.DIRECT_17;
-        this._numberToSectionMapping[18] = we.ro.BetField.DIRECT_18;
-        this._numberToSectionMapping[19] = we.ro.BetField.DIRECT_19;
-        this._numberToSectionMapping[20] = we.ro.BetField.DIRECT_20;
-        this._numberToSectionMapping[21] = we.ro.BetField.DIRECT_21;
-        this._numberToSectionMapping[22] = we.ro.BetField.DIRECT_22;
-        this._numberToSectionMapping[23] = we.ro.BetField.DIRECT_23;
-        this._numberToSectionMapping[24] = we.ro.BetField.DIRECT_24;
-        this._numberToSectionMapping[25] = we.ro.BetField.DIRECT_25;
-        this._numberToSectionMapping[26] = we.ro.BetField.DIRECT_26;
-        this._numberToSectionMapping[27] = we.ro.BetField.DIRECT_27;
-        this._numberToSectionMapping[28] = we.ro.BetField.DIRECT_28;
-        this._numberToSectionMapping[29] = we.ro.BetField.DIRECT_29;
-        this._numberToSectionMapping[30] = we.ro.BetField.DIRECT_30;
-        this._numberToSectionMapping[31] = we.ro.BetField.DIRECT_31;
-        this._numberToSectionMapping[32] = we.ro.BetField.DIRECT_32;
-        this._numberToSectionMapping[33] = we.ro.BetField.DIRECT_33;
-        this._numberToSectionMapping[34] = we.ro.BetField.DIRECT_34;
-        this._numberToSectionMapping[35] = we.ro.BetField.DIRECT_35;
-        this._numberToSectionMapping[36] = we.ro.BetField.DIRECT_36;
-
         this._betChipStackMapping = {};
         this._betChipStackMapping[we.ro.BetField.DIRECT_0] = this._section_0_betChipStack;
         this._betChipStackMapping[we.ro.BetField.DIRECT_1] = this._section_1_betChipStack;
@@ -264,6 +183,18 @@ namespace we {
         this._betChipStackMapping[ro.BetField.ZERO_GAME] = this._section_zero_betChipStack;
       }
 
+      public removeAllMouseListeners() {
+        this.removeRolloverListeners();
+        this.removeRolloutListeners();
+        this.removeTouchTapListeners();
+      }
+
+      public addAllMouseListeners() {
+        this.addRolloverListeners();
+        this.addRolloutListeners();
+        this.addTouchTapListeners();
+      }
+
       public onGridRollover(fieldName: string) {
         switch (fieldName) {
           case ro.BetField.ORPHANS:
@@ -273,8 +204,8 @@ namespace we {
             this._raceTrackTableLayer.onRollover(fieldName);
             break;
           default:
-            we.ro.getNeighbour(this._sectionToNumberMapping[fieldName], this._raceTrackControl.value).map(value => {
-              this._raceTrackTableLayer.onRollover(this._numberToSectionMapping[value]);
+            we.ro.getNeighbour(ro.directfield2num(fieldName), this._raceTrackControl.value).map(value => {
+              this._raceTrackTableLayer.onRollover(ro.num2directfield(value));
             });
             break;
         }
@@ -289,8 +220,8 @@ namespace we {
             this._raceTrackTableLayer.onRollout(fieldName);
             break;
           default:
-            we.ro.getNeighbour(this._sectionToNumberMapping[fieldName], this._raceTrackControl.value).map(value => {
-              this._raceTrackTableLayer.onRollout(this._numberToSectionMapping[value]);
+            we.ro.getNeighbour(ro.directfield2num(fieldName), this._raceTrackControl.value).map(value => {
+              this._raceTrackTableLayer.onRollout(ro.num2directfield(value));
             });
             break;
         }
@@ -307,8 +238,8 @@ namespace we {
             this.onBetFieldUpdate(fieldName);
             break;
           default:
-            we.ro.getNeighbour(this._sectionToNumberMapping[fieldName], this._raceTrackControl.value).map(value => {
-              this.onBetFieldUpdate(this._numberToSectionMapping[value]);
+            we.ro.getNeighbour(ro.directfield2num(fieldName), this._raceTrackControl.value).map(value => {
+              this.onBetFieldUpdate(ro.num2directfield(value));
             });
             break;
         }
