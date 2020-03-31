747b9af09eca57007cf7dc564403a472b9fc32f5
Joe Ng <joesh1203@gmail.com>
2020-03-31 21:26:56 +0800

    - update di GameResultMessage

--- src/game/di/component/GameResultMessage.ts
+++ src/game/di/component/GameResultMessage.ts
@@ -21,47 +21,62 @@ namespace we {
           this
         );
 
-        const { dice1, dice2, dice3, size, odd, total } = <di.GameData>gameData;
+        const { dice1, dice2, dice3, size, odd, total } = <di.GameData> gameData;
         logger.l(dice1, dice2, dice3, size, odd, total);
 
         let anim = 'ani_result_';
+        let txtSlot = null;
         let isWin = false;
         if (isNaN(winAmount)) {
           anim += 'no_bets';
         } else if (winAmount <= 0) {
           anim += 'loss';
+          txtSlot = 'loss_txt';
         } else {
           anim += 'win';
+          txtSlot = 'L1_txt';
           isWin = true;
         }
 
+        const diceResults = [dice1, dice1, dice2, dice3];
         for (let i = 1; i <= 3; i += 1) {
           const slot = this._display.armature.getSlot(`dice_${i + (isWin ? 6 : 9)}`);
           const img = new eui.Image();
-          img.source = RES.getRes(`d_sic_history_lv3_dice-${i}_png`);
+          img.source = `d_sic_history_lv3_dice-${diceResults[i]}_png`; // RES.getRes(`d_sic_history_lv3_dice-${diceResults[i]}_png`);
           // cannot use image.width
           img.anchorOffsetX = 27;
           img.anchorOffsetY = 27;
           slot.display = img;
         }
 
-        const array = [
-          [isWin ? '15' : '16', 60, total.toString()],
-          [isWin ? 'red_txt2' : 'red_txt3', 40, size === 1 ? '小' : '大'],
-          [isWin ? 'blue_txt2' : 'blue_txt3', 40, odd === 1 ? '單' : '雙'],
-        ];
+        const array = [[isWin ? '15' : '16', 60, total.toString()], [isWin ? 'red_txt2' : 'red_txt3', 40, size === 1 ? '小' : '大'], [isWin ? 'blue_txt2' : 'blue_txt3', 40, odd === 1 ? '單' : '雙']];
 
         for (const [slotName, fontSize, text] of array) {
-          const slot = this._display.armature.getSlot(<string>slotName);
+          const slot = this._display.armature.getSlot(<string> slotName);
           const r = new eui.Label();
           r.fontFamily = 'Barlow';
-          r.size = <number>fontSize;
-          r.text = <string>text;
+          r.size = <number> fontSize;
+          r.text = <string> text;
           if (fontSize === 60) {
             const shadowFilter: egret.DropShadowFilter = new egret.DropShadowFilter(3, 45, 0x111111, 0.1, 10, 10, 20, egret.BitmapFilterQuality.LOW);
             r.filters = [shadowFilter];
             r.bold = true;
           }
+          r.textColor = 0xffffff;
+          r.anchorOffsetX = r.width / 2;
+          r.anchorOffsetY = r.height / 2;
+          slot.display = r;
+        }
+
+        if (txtSlot) {
+          const slot = this._display.armature.getSlot(txtSlot);
+          const r = new eui.Label();
+          r.fontFamily = 'Barlow';
+          r.size = 60;
+          r.text = utils.formatNumber(winAmount);
+          const shadowFilter: egret.DropShadowFilter = new egret.DropShadowFilter(3, 45, 0x111111, 0.1, 10, 10, 20, egret.BitmapFilterQuality.LOW);
+          r.filters = [shadowFilter];
+          r.bold = true;
           r.textColor = 0xffffff;
           r.anchorOffsetX = r.width / 2;
           r.anchorOffsetY = r.height / 2;
