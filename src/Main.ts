8ade000de84a645655026daa5ab79a83fb361a16
Joe Ng <joe.dev@pgpg.la>
2020-04-01 16:06:07 +0800

    - Removed ImageButtonSkinLobby default state setting - Reset Main.ts mobile checking

--- src/Main.ts
+++ src/Main.ts
@@ -51,8 +51,8 @@ class Main extends eui.UILayer {
     document.documentElement.className = cn.join(' ');
 
     const { type } = env.UAInfo.device;
-    // if (type === 'mobile') {
-    if (true) {
+    if (type === 'mobile') {
+      // if (true) {
       env.isMobile = true;
       this.updateMobileHitTest();
       // use these when there is portrait mode only
@@ -115,10 +115,7 @@ class Main extends eui.UILayer {
       await RES.loadConfig(`resource/default.res.json`, 'resource/');
       await RES.loadConfig(`resource/${env.isMobile ? 'mobile' : 'desktop'}.res.json`, 'resource/');
       await this.loadTheme();
-      fontMgr.loadFonts([
-        { res: 'Barlow-Regular', name: 'Barlow' },
-        { res: 'BarlowCondensed-SemiBold', name: 'BarlowCondensed' },
-      ]);
+      fontMgr.loadFonts([{ res: 'Barlow-Regular', name: 'Barlow' }, { res: 'BarlowCondensed-SemiBold', name: 'BarlowCondensed' }]);
       await RES.loadGroup(we.core.res.EgretBasic);
     } catch (err) {
       logger.e(err);
