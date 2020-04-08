919424741395f6556834b07c0dff1e32e2f39deb
Fung Yau <ycfung93@gmail.com>
2020-04-08 20:53:22 +0800

    - Add MobileBottomCommonPanel and MobileBottomRoadmapPanel for ba mobilebottomgamepanel

---
+++ src/ui/common/mobile/MobileBottomCommonPanel.ts
@@ -0,0 +1,131 @@
+namespace we {
+  export namespace ui {
+    export class MobileBottomCommonPanel extends core.BaseGamePanel {
+
+      protected gameIdLabel: ui.RunTimeLabel;
+      protected totalBetLabel: ui.RunTimeLabel;
+      protected gameId: string;
+      protected totalBet: number;
+
+      protected _arrow: egret.DisplayObject;
+      protected _arrowUp: egret.DisplayObject;
+      public isPanelOpen: boolean = true;
+
+      // table info panel
+      public _tableInfoPanel: ba.TableInfoPanel;
+      public _betLimitDropDownBtn: ui.RunTimeLabel;
+
+      // statisticChartPanel
+      public _statisticChartPanel: ba.StatisticChartPanel;
+
+      // viewStack and radioBtn
+      protected _roadmapGroup: eui.Group;
+      protected _infoGroup: eui.Group;
+
+      protected roadSheetBtn: eui.RadioButton;
+      protected chartBtn: eui.RadioButton;
+      protected tableInfoBtn: eui.RadioButton;
+
+      protected _gameInfoLabel: ui.RunTimeLabel;
+
+      protected viewStack: eui.ViewStack;
+      protected viewStackMask: eui.Rect;
+
+      protected _verGroup: eui.Group;
+
+      public constructor(skin?: string) {
+        super(skin || !env.isMobile ? skin : 'ba.MobileBottomGamePanel');
+      }
+
+      protected init() {
+        this.gameId = '';
+        this.totalBet = 0;
+        this._betLimitDropDownBtn = this._tableInfoPanel.pBetLimit;
+
+        this.roadSheetBtn.addEventListener(eui.UIEvent.CHANGE, this.onViewChange, this);
+        this.chartBtn.addEventListener(eui.UIEvent.CHANGE, this.onViewChange, this);
+        this.tableInfoBtn.addEventListener(eui.UIEvent.CHANGE, this.onViewChange, this);
+
+        dir.evtHandler.addEventListener(core.Event.SWITCH_LANGUAGE, this.changeLang, this);
+        this.changeLang();
+
+        this._arrow.addEventListener(egret.TouchEvent.TOUCH_TAP, this.onPanelOpen, this);
+        this._arrowUp.addEventListener(egret.TouchEvent.TOUCH_TAP, this.onPanelOpen, this);
+
+        this.viewStack.mask = this.viewStackMask;
+        this.createVerLayout();
+
+        this.viewStack.selectedIndex = 0;
+
+        this.onPanelOpen();
+      }
+
+      public changeLang() {
+        this.gameIdLabel.text = this.gameId + ' ' + i18n.t('baccarat.gameroundid');
+        this.totalBetLabel.text = i18n.t('baccarat.totalbet') + ' ' + this.totalBet;
+
+        this.roadSheetBtn.label = i18n.t('mobile_game_panel_road_sheet');
+        this.chartBtn.label = i18n.t('mobile_game_panel_statistic_chart');
+        this.tableInfoBtn.label = i18n.t('mobile_game_panel_table_info');
+        this._gameInfoLabel.text = i18n.t('mobile_panel_game_Info');
+      }
+
+      protected onPanelOpen() {
+        this.roadSheetBtn.visible = !this.isPanelOpen;
+        this.chartBtn.visible = !this.isPanelOpen;
+        this.tableInfoBtn.visible = !this.isPanelOpen;
+        this._gameInfoLabel.visible = this.isPanelOpen;
+        this._arrow.visible = !this.isPanelOpen;
+        this._arrowUp.visible = this.isPanelOpen;
+
+        if (this.isPanelOpen) {
+          this.isPanelOpen = false;
+          egret.Tween.get(this.viewStack).to({ height: 0 }, 250);
+          egret.Tween.get(this.viewStackMask).to({ height: 0 }, 250);
+        } else {
+          this.isPanelOpen = true;
+          egret.Tween.get(this.viewStack).to({ height: 532 }, 250);
+          egret.Tween.get(this.viewStackMask).to({ height: 532 }, 250);
+        }
+      }
+
+      protected onViewChange(e: eui.UIEvent) {
+        const radio: eui.RadioButton = e.target;
+        this.viewStack.selectedIndex = radio.value;
+      }
+
+      protected createVerLayout() {
+        const vLayout: eui.VerticalLayout = new eui.VerticalLayout();
+        vLayout.horizontalAlign = egret.HorizontalAlign.CENTER;
+        vLayout.gap = 0;
+        this._verGroup.layout = vLayout;
+      }
+
+
+      public update() {
+        if (this.tableInfo) {
+          if (this.tableInfo.betInfo) {
+            if (this.tableInfo.betInfo.gameroundid) {
+              this.gameId = this.tableInfo.betInfo.gameroundid;
+            }
+            if (this.tableInfo.betInfo.total) {
+              this.totalBet = this.tableInfo.betInfo.total;
+            }
+          }
+          this.changeLang();
+        }
+      }
+
+      public destroy() {
+        super.destroy();
+
+        if (dir.evtHandler.hasEventListener(core.Event.SWITCH_LANGUAGE)) {
+          dir.evtHandler.removeEventListener(core.Event.SWITCH_LANGUAGE, this.changeLang, this);
+        }
+
+        this._arrow.removeEventListener(egret.TouchEvent.TOUCH_TAP, this.onPanelOpen, this);
+        this._arrowUp.removeEventListener(egret.TouchEvent.TOUCH_TAP, this.onPanelOpen, this);
+      }
+    }
+  }
+}
