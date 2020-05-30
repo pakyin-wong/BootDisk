9429468c929a8723cb0e0c3a8fc6a3e5272dc0a3
joengsh <joengsh@gmail.com>
2020-05-30 09:22:07 +0800

    - clear the viewStack in RoRightPanel when destroy - dispose db display when destroy in BaseAnimationButton

--- src/game/ro/components/RoRightPanel.ts
+++ src/game/ro/components/RoRightPanel.ts
@@ -43,25 +43,25 @@ namespace we {
         this._distributionLabel.y = 240;
         this._distributionLabel.right = 400;
 
-        // this._raceTrackControl = new RaceTrackControl();
-        // this._raceTrackControl.horizontalCenter = 0;
-        // this._raceTrackControl.y = 220;
+        this._raceTrackControl = new RaceTrackControl();
+        this._raceTrackControl.horizontalCenter = 0;
+        this._raceTrackControl.y = 220;
 
         this._raceTrackTableLayer = new RaceTrackTableLayer();
         this._raceTrackTableLayer.horizontalCenter = 0;
         this._raceTrackTableLayer.y = 20;
 
-        // this._raceTrackChipLayer = new RaceTrackChipLayer();
-        // this._raceTrackChipLayer.horizontalCenter = 0;
-        // this._raceTrackChipLayer.y = 20;
+        this._raceTrackChipLayer = new RaceTrackChipLayer();
+        this._raceTrackChipLayer.horizontalCenter = 0;
+        this._raceTrackChipLayer.y = 20;
 
-        // this._raceTrackChipLayer.raceTrackTableLayer = this._raceTrackTableLayer;
-        // this._raceTrackChipLayer.raceTrackControl = this._raceTrackControl;
-        // this._raceTrackChipLayer.chipLayer = chipLayer; //set rochiplayer into race chiplayer
+        this._raceTrackChipLayer.raceTrackTableLayer = this._raceTrackTableLayer;
+        this._raceTrackChipLayer.raceTrackControl = this._raceTrackControl;
+        this._raceTrackChipLayer.chipLayer = chipLayer; // set rochiplayer into race chiplayer
 
         page1Group.addChild(this._raceTrackTableLayer);
-        // page1Group.addChild(this._raceTrackChipLayer);
-        // page1Group.addChild(this._raceTrackControl);
+        page1Group.addChild(this._raceTrackChipLayer);
+        page1Group.addChild(this._raceTrackControl);
         page1Group.addChild(this._distributionLabel);
       }
 
@@ -132,6 +132,12 @@ namespace we {
 
       public destroy() {
         super.destroy();
+
+        const page1Group = this.pageStack.getChildAt(0) as eui.Group;
+        page1Group.removeChildren();
+
+        const page2Group = this.pageStack.getChildAt(1) as eui.Group;
+        page2Group.removeChildren();
 
         // this.beadRoad.dispose();
         egret.Tween.removeTweens(this.activeLine);
