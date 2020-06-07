9e7ef5b24849a1de610a72a1a39127e6bd9ff2bd
joengsh <joengsh@gmail.com>
2020-06-07 12:46:00 +0800

    - lossy compress nmimages

--- texturepack.sh
+++ texturepack.sh
@@ -6,9 +6,8 @@ cd ..
 echo packing completed
 echo Lossless compress images at resource/assets/spritesheet...
 optipng resource/assets/spritesheet/**/**/*.png
-optipng resource/assets/nmimages/**/**/*.png
-optipng resource/assets/nmimages/**/**/**/*.png
-optipng resource/assets/nmimages/**/**/**/**/*.png
+pngquant --force --ext .png --skip-if-larger resource/assets/nmimages/**/**/*.png
+jpegoptim --max=70 resource/assets/nmimages/**/*.jpg
 optipng resource/assets/dragonbones/**/*.png
 optipng resource/assets/dragonbones/**/**/*.png
 optipng resource/assets/dragonbones/**/**/**/*.png
