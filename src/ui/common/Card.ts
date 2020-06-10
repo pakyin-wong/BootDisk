f287f31fd277c0f4674adb53c68e6faf752072f9
Pakyin Wong <pakyinwww@gmail.com>
2020-06-10 13:34:33 +0800

    Remove horizontal card state

--- src/ui/common/Card.ts
+++ src/ui/common/Card.ts
@@ -7,52 +7,19 @@ namespace we {
       protected childrenCreated() {
         super.childrenCreated();
       }
-      public setCard(resName: string, vertical: boolean) {
+      public setCard(resName: string, vertical: boolean = true) {
         this.removeChildren();
-        // console.log('resName', resName);
         if (resName) {
           const card: egret.Bitmap = new egret.Bitmap();
-          // console.log(resName);
-          const texName = `d_common_poker_${vertical ? 'vertical' : 'horizontal'}_${resName}_png`;
+          const texName = `d_common_poker_vertical_${resName}_png`;
 
-          // card.texture = RES.getRes(core.Card[resName]);
           card.texture = RES.getRes(texName);
-          if (vertical) {
-            card.width = this.width;
-            card.height = this.height;
-          } else {
-            // console.log('horizontal');
-            // card.rotation = 90;
-            // card.texture = RES.getRes(core.Card[resName]);
-            card.texture = RES.getRes(texName);
-            card.width = this.width;
-            card.height = this.height;
-            // card.width = this.height;
-            // card.height = this.width;
-            // card.x = 0 + this.width;
-            // card.y = 0;
-          }
+          card.width = this.width;
+          card.height = this.height;
+
           this.addChild(card);
         }
       }
-
-      /*
-      public setHCard(resName: string) {
-        this.removeChildren()
-              if (resName){
-        const card: egret.Bitmap = new egret.Bitmap();
-        console.log(resName);
-        card.rotation = 90;
-        card.texture = RES.getRes(enums.card[resName]);
-        card.width = this.height;
-        card.height = this.width;
-        card.x = 0 + this.width;
-        card.y = 0;
-
-        this.addChild(card);
-              }
-      }
-      */
 
       public clear() {
         this.removeChildren();
