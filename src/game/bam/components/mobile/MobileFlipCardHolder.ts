ddc78a7b6fee5aada7ab4c9644abb713dd761ec5
Fung Yau <ycfung93@gmail.com>
2020-06-18 18:21:00 +0800

    - update m bam

--- src/game/bam/components/mobile/MobileFlipCardHolder.ts
+++ src/game/bam/components/mobile/MobileFlipCardHolder.ts
@@ -4,8 +4,10 @@ namespace we {
       public _flipCard1: ba.FlipCard;
       public _flipCard2: ba.FlipCard;
 
-      public showCardIndex: number = 0;
+      protected currentCardIndex: number = 0;
 
+      protected currentCard;
+      protected nextCard;
       protected isOpen: boolean;
 
       protected leftStartPosition: number;
@@ -19,31 +21,40 @@ namespace we {
       protected createChildren() {
         super.createChildren();
         this.skinName = utils.getSkinByClassname('bam.FlipCardHolderSkin');
+        this.leftStartPosition = -1289;
+        this.rightStartPosition = 189;
+        this.middleStartPosition = 1289;
       }
 
       protected childrenCreated() {
         super.childrenCreated();
       }
 
-      protected moveCard() {
-        // egret.Tween.get(this._flipCard).to(
-        //   {
-        //     x: this.slideWidth,
-        //   },
-        //   duration
-        // );
-        // egret.Tween.get(current)
-        //   .to(
-        //     {
-        //       x: 0,
-        //     },
-        //     duration
-        //   )
-        //   .call(this.onMoveFinished, this, [0]);
+      protected updateCardPos(startPos: string){
+        if(startPos == 'left'){
+          this.nextCard.x = this.leftStartPosition;
+        }
+        else if(startPos == 'right'){
+          this.nextCard.x = this.rightStartPosition;
+        }
+      }
+
+      protected showAndMoveCard(index: number, value: string) {
+        if(this.currentCardIndex == index){
+          return;
+        }
+
+        if(this.currentCardIndex < index){
+          //右至左
+        }
+
+        if(this.currentCardIndex > index){
+          //左至右
+        }
       }
 
       public setCardImage(index: number, value: string) {
-        this.showCardIndex = index;
+        this.currentCardIndex = index;
 
         console.log('flipcardpath', `d_sq_bac_large_poker_${utils.formatCardForFlip(value)}_png`);
         this._flipCard1.setCardImage('m_sq_ba_large_poker_backside_png', `m_sq_bac_large_poker_${utils.formatCardForFlip(value)}_png`, `m_sq_bac_large_poker_${utils.formatCardForFlip(value)}_png`);
