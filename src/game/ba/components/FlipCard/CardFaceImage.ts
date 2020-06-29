a251359527925d658e270a4925fd1eab5cf716e7
fungdev2020 <fung.dev@pgpg.la>
2020-06-29 19:11:45 +0800

    - update m ver flip card mask

--- src/game/ba/components/FlipCard/CardFaceImage.ts
+++ src/game/ba/components/FlipCard/CardFaceImage.ts
@@ -16,7 +16,11 @@ namespace we {
         this.addChild(this.cardImage);
 
         this.cardFaceNumberMask = new eui.Image();
-        this.cardFaceNumberMask.source = 'd_sq_ba_large_poker_card_mask_png';
+        if (!env.isMobile) {
+          this.cardFaceNumberMask.source = 'd_sq_ba_large_poker_card_mask_png';
+        } else {
+          this.cardFaceNumberMask.source = 'm_sq_ba_large_poker_card_mask_png';
+        }
         this.cardFaceNumberMask.height = h;
         this.cardFaceNumberMask.width = w;
         this.addChild(this.cardFaceNumberMask);
