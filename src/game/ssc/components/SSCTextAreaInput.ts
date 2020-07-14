fd297a390a67643c5a39f14fe547009ec05c2969
jackydevpgpg <jacky.dev@pgpg.la>
2020-07-14 20:49:23 +0800

    update text area validation

--- src/game/ssc/components/SSCTextAreaInput.ts
+++ src/game/ssc/components/SSCTextAreaInput.ts
@@ -8,6 +8,7 @@ namespace we {
       protected _instruction: ui.RunTimeLabel;
 
       protected _btnUpload;
+      protected _btnFix;
       constructor(index: number, config: any) {
         super(index, config);
         this.skinName = 'skin_desktop.lo.SSCTextAreaInput';
@@ -15,32 +16,42 @@ namespace we {
 
       protected initComponents() {
         this._btnUpload.addEventListener(egret.TouchEvent.TOUCH_TAP, this.onTouchUpload, this);
-        // this._textArea = new egret.TextField();
-        // this._textArea.width = 1408;
-        // this._textArea.height = 180;
-        // this._textArea.background = true;
-        // this._textArea.backgroundColor = 0x1b232c;
-        // this._textArea.multiline = true;
-        // this._textArea.setFocus();
-        // this._textAreaGroup.addChild(this._textArea);
+        this._btnFix.addEventListener(egret.TouchEvent.TOUCH_TAP, this.onTouchFix, this);
+
       }
 
+      protected onTextAreaChange(){
+        
+      }
+      // update the data when user interact with the component
+      protected updateData() {
+
+        //this._data = '';
+
+        // this.dispatchEventWith(egret.Event.CHANGE, false, { index: this._index, data: this._data });
+      }
+      
       protected onTouchUpload(e: egret.TouchEvent) {
         const uploadText: any = document.getElementById('uploadText');
-        uploadText.onchange = this.onChange.bind(this);
+        uploadText.onchange = this.onUploadChange.bind(this);
         uploadText.click();
       }
 
-      private onChange() {
+      protected onTouchFix(e: egret.TouchEvent) {
+        const inputText = this._textArea.text;
+        this.validateTextArea(inputText);
+      }
+
+      private onUploadChange() {
         // 获取选择图片
         const uploadText: any = document.getElementById('uploadText');
         const file = uploadText.files[0];
         // 判断图片类型
-        // var imageType = /^image\//;
-        // if(!imageType.test(file.type)) {
-        //     alert("请选择图片类型上传");
-        //     return;
-        // }
+         const type = /[^text/plain]/;
+         if(type.test(file.type)) {
+             alert("请选择TXT类型上传");
+             return;
+         }
         // 加载图片
         const reader = new FileReader();
 
@@ -56,20 +67,63 @@ namespace we {
       public loadFileComplete(result): any {
         // 将加载图片的数据赋值给myImg
         // this._textArea.addEventListener(egret.Event.COMPLETE, this.onMyImgComplete, this);
+        this._textArea.text = '';
         this._textArea.text = atob(result.split(',')[1]);
-        console.log('hi');
-      }
-      public atou(str) {
-        return decodeURIComponent(atob(str));
       }
 
-      protected onTextInputUpdate(e: egret.Event) {
-        this._data = this.validateTextArea(this._textArea.text);
-      }
+      // protected onTextInputUpdate(e: egret.Event) {
+      //   this._data = this.validateTextArea(this._textArea.text);
 
-      protected validateTextArea(text) {
-        // validate via rule
-        return text;
+      // }
+
+      protected validateTextArea(text: string) {
+        // remove except numbers
+        const numberPerGroup = this._config.numberPerGroup;
+        const punctuationRegex = /[^0-9]/gi;
+        let validateText = text.trim();
+
+        const checkArray = validateText.split('');
+
+        for (let i = 0; i < validateText.length; i++) {
+          if (validateText[i].search(punctuationRegex) > -1) {
+            checkArray[i] = '';
+          }
+        }
+
+        let check = '';
+        for (let i = 0; i < checkArray.length; i++) {
+          check += checkArray[i];
+        }
+
+        validateText = check.replace(/\s/, '');
+        // split to datas
+
+        const datas = validateText.split('');
+        const tempDatas = [];
+        let temp = '';
+
+        this._data = '';
+        this._textArea.text = '';
+        let count = 0;
+        
+        for (let i = 0; i < datas.length; i++) {
+          temp += datas[i];
+          count ++;
+          if (count % numberPerGroup === 0) {
+            tempDatas.push(temp);
+            temp = '';
+          }
+        }
+
+        for (let i = 0; i < tempDatas.length; i++) {
+          if (i === tempDatas.length - 1) {
+            this._textArea.text += tempDatas[i];
+            this._data += tempDatas[i];
+          } else {
+            this._textArea.text += tempDatas[i]  + '|';
+            this._data += tempDatas[i]  + ', ';
+          }
+        }
       }
     }
   }
