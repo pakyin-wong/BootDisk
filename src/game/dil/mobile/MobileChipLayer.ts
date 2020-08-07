d496b42cc43574bfcff62b4c4d5c70f0d0546020
Fung Yau <ycfung93@gmail.com>
2020-08-07 15:33:49 +0800

    - update dil m ver

--- src/game/dil/mobile/MobileChipLayer.ts
+++ src/game/dil/mobile/MobileChipLayer.ts
@@ -1,143 +1,10 @@
 namespace we {
   export namespace dil {
-    export class MobileChipLayer extends we.ui.ChipLayer {
-      protected _flashingOdd: eui.Label;
-      protected _luckyAnims: dragonBones.EgretArmatureDisplay[];
-      protected _winningAnim: dragonBones.EgretArmatureDisplay;
-
-      // group
-      protected _sum_3_group: eui.Group;
-      protected _sum_4_group: eui.Group;
-      protected _sum_5_group: eui.Group;
-      protected _sum_6_group: eui.Group;
-      protected _sum_7_group: eui.Group;
-      protected _sum_8_group: eui.Group;
-      protected _sum_9_group: eui.Group;
-      protected _sum_10_group: eui.Group;
-      protected _sum_11_group: eui.Group;
-      protected _sum_12_group: eui.Group;
-      protected _sum_13_group: eui.Group;
-      protected _sum_14_group: eui.Group;
-      protected _sum_15_group: eui.Group;
-      protected _sum_16_group: eui.Group;
-      protected _sum_17_group: eui.Group;
-      protected _sum_18_group: eui.Group;
-
-      protected _sum_3_betChipStack: ui.BetChipStack;
-      protected _sum_4_betChipStack: ui.BetChipStack;
-      protected _sum_5_betChipStack: ui.BetChipStack;
-      protected _sum_6_betChipStack: ui.BetChipStack;
-      protected _sum_7_betChipStack: ui.BetChipStack;
-      protected _sum_8_betChipStack: ui.BetChipStack;
-      protected _sum_9_betChipStack: ui.BetChipStack;
-      protected _sum_10_betChipStack: ui.BetChipStack;
-      protected _sum_11_betChipStack: ui.BetChipStack;
-      protected _sum_12_betChipStack: ui.BetChipStack;
-      protected _sum_13_betChipStack: ui.BetChipStack;
-      protected _sum_14_betChipStack: ui.BetChipStack;
-      protected _sum_15_betChipStack: ui.BetChipStack;
-      protected _sum_16_betChipStack: ui.BetChipStack;
-      protected _sum_17_betChipStack: ui.BetChipStack;
-      protected _sum_18_betChipStack: ui.BetChipStack;
+    export class MobileChipLayer extends we.dil.ChipLayer {
 
       constructor() {
         super();
         this._betField = dil.BetField;
-      }
-
-      protected createMapping() {
-        super.createMapping();
-        this._mouseAreaMapping = {};
-        this._mouseAreaMapping[dil.BetField.SUM_3] = this._sum_3_group;
-        this._mouseAreaMapping[dil.BetField.SUM_4] = this._sum_4_group;
-        this._mouseAreaMapping[dil.BetField.SUM_5] = this._sum_5_group;
-        this._mouseAreaMapping[dil.BetField.SUM_6] = this._sum_6_group;
-        this._mouseAreaMapping[dil.BetField.SUM_7] = this._sum_7_group;
-        this._mouseAreaMapping[dil.BetField.SUM_8] = this._sum_8_group;
-        this._mouseAreaMapping[dil.BetField.SUM_9] = this._sum_9_group;
-        this._mouseAreaMapping[dil.BetField.SUM_10] = this._sum_10_group;
-        this._mouseAreaMapping[dil.BetField.SUM_11] = this._sum_11_group;
-        this._mouseAreaMapping[dil.BetField.SUM_12] = this._sum_12_group;
-        this._mouseAreaMapping[dil.BetField.SUM_13] = this._sum_13_group;
-        this._mouseAreaMapping[dil.BetField.SUM_14] = this._sum_14_group;
-        this._mouseAreaMapping[dil.BetField.SUM_15] = this._sum_15_group;
-        this._mouseAreaMapping[dil.BetField.SUM_16] = this._sum_16_group;
-        this._mouseAreaMapping[dil.BetField.SUM_17] = this._sum_17_group;
-        this._mouseAreaMapping[dil.BetField.SUM_18] = this._sum_18_group;
-
-        Object.keys(this._mouseAreaMapping).map(value => {
-          console.log(value);
-          mouse.setButtonMode(this._mouseAreaMapping[value], true);
-        });
-
-        this._betChipStackMapping = {};
-        this._betChipStackMapping[dil.BetField.SUM_3] = this._sum_3_betChipStack;
-        this._betChipStackMapping[dil.BetField.SUM_4] = this._sum_4_betChipStack;
-        this._betChipStackMapping[dil.BetField.SUM_5] = this._sum_5_betChipStack;
-        this._betChipStackMapping[dil.BetField.SUM_6] = this._sum_6_betChipStack;
-        this._betChipStackMapping[dil.BetField.SUM_7] = this._sum_7_betChipStack;
-        this._betChipStackMapping[dil.BetField.SUM_8] = this._sum_8_betChipStack;
-        this._betChipStackMapping[dil.BetField.SUM_9] = this._sum_9_betChipStack;
-        this._betChipStackMapping[dil.BetField.SUM_10] = this._sum_10_betChipStack;
-        this._betChipStackMapping[dil.BetField.SUM_11] = this._sum_11_betChipStack;
-        this._betChipStackMapping[dil.BetField.SUM_12] = this._sum_12_betChipStack;
-        this._betChipStackMapping[dil.BetField.SUM_13] = this._sum_13_betChipStack;
-        this._betChipStackMapping[dil.BetField.SUM_14] = this._sum_14_betChipStack;
-        this._betChipStackMapping[dil.BetField.SUM_15] = this._sum_15_betChipStack;
-        this._betChipStackMapping[dil.BetField.SUM_16] = this._sum_16_betChipStack;
-        this._betChipStackMapping[dil.BetField.SUM_17] = this._sum_17_betChipStack;
-        this._betChipStackMapping[dil.BetField.SUM_18] = this._sum_18_betChipStack;
-      }
-
-      protected isExceedBetLimit(fieldAmounts: {}, betLimit: data.BetLimitSet) {
-        for (const key of Object.keys(fieldAmounts)) {
-          if (fieldAmounts[key] > betLimit.maxlimit) {
-            return true;
-          }
-        }
-        return false;
-      }
-
-      protected addGridBg(grid: any, num: number) {
-        return (evt: dragonBones.EgretEvent) => {
-          if (!evt && !evt.eventObject && evt.eventObject.name !== 'INSERT_GRID_BG') {
-            return;
-          }
-        };
-      }
-
-      public clearLuckyNumber() {
-        this.clearLuckyAnim();
-        this.clearWinningAnim();
-      }
-
-      public clearWinningAnim() {
-        if (!this._winningAnim || !this._winningAnim.parent) {
-          return;
-        }
-        this._winningAnim.parent.removeChild(this._winningAnim);
-      }
-
-      public clearLuckyAnim() {
-        if (!this._luckyAnims) {
-          return;
-        }
-        this._luckyAnims.map(luckyAnim => {
-          if (!luckyAnim || !luckyAnim.parent) {
-            return;
-          }
-
-          luckyAnim.animation.stop();
-          if (luckyAnim.parent.contains(luckyAnim)) {
-            luckyAnim.dispose();
-            luckyAnim.parent.removeChild(luckyAnim);
-          }
-        });
-
-        if (this._flashingOdd && this._flashingOdd.parent) {
-          egret.Tween.removeTweens(this._flashingOdd);
-          this._flashingOdd.parent.removeChild(this._flashingOdd);
-        }
       }
 
       public showWinningNumber() {
@@ -163,7 +30,18 @@ namespace we {
         this.clearWinningAnim();
         this.clearLuckyAnim();
 
-        this._winningAnim = this.createAnim('bet_effect');
+        let animString = '';
+
+        if(env.orientation === 'portrait')
+        {
+          animString = 'Bet_Effect_Mobile_Virtical';
+        }
+        else
+        {
+          animString = 'Bet_Effect_Mobile_Horizontal';
+        }
+
+        this._winningAnim = this.createAnim(animString);
         const grid = this._mouseAreaMapping[dil.BetField['SUM_' + sum]];
         grid.addChild(this._winningAnim);
         this._winningAnim.anchorOffsetX = 3;
@@ -212,7 +90,18 @@ namespace we {
 
           const grid = this._mouseAreaMapping[dil.BetField['SUM_' + key]];
 
-          const luckyAnim = this.createAnim('bet_effect');
+          let animString = '';
+
+          if(env.orientation === 'portrait')
+          {
+            animString = 'Bet_Effect_Mobile_Virtical';
+          }
+          else
+          {
+            animString = 'Bet_Effect_Mobile_Horizontal';
+          }
+
+          const luckyAnim = this.createAnim(animString);
           luckyAnim.addDBEventListener(dragonBones.EventObject.FRAME_EVENT, this.addGridBg(grid, +key), luckyAnim);
 
           grid.addChild(luckyAnim);
@@ -252,18 +141,6 @@ namespace we {
             await p;
           })();
         });
-      }
-
-      protected restructureChildren() {}
-
-      protected createAnim(armatureName: string) {
-        const skeletonData = RES.getRes(`dice_w_game_result_ske_json`);
-        const textureData = RES.getRes(`dice_w_game_result_tex_json`);
-        const texture = RES.getRes(`dice_w_game_result_tex_png`);
-        const factory = new dragonBones.EgretFactory();
-        factory.parseDragonBonesData(skeletonData);
-        factory.parseTextureAtlasData(textureData, texture);
-        return factory.buildArmatureDisplay(armatureName);
       }
     }
   }
