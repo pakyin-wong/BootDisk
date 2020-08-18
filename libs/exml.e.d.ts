21b5821101e546b9eef1f67fbec6f2379de62fd8
kenLCH <ken.dev@pgpg.la>
2020-08-18 17:21:13 +0800

    -update am mobile p countdownlabe size and open all style

--- libs/exml.e.d.ts
+++ libs/exml.e.d.ts
@@ -2091,6 +2091,10 @@ declare module skin_mobile{
 	}
 }
 declare module skin_mobile{
+	class CountdownTimerRoundSqueezeBaccaratSkin extends eui.Skin{
+	}
+}
+declare module skin_mobile{
 	class MobileSideGameListItemRenderer extends eui.Skin{
 	}
 }
