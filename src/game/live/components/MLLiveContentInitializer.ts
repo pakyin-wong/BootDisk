7f639d346723a73454672d579a5050211050f37f
kenLCH <ken.dev@pgpg.la>
2020-08-24 19:04:52 +0800

    -exchange imageslider into horizontalHolder

--- src/game/live/components/MLLiveContentInitializer.ts
+++ src/game/live/components/MLLiveContentInitializer.ts
@@ -9,11 +9,30 @@ namespace we {
       public initContent(root: GameTableList) {
         this.root = root;
 
-        root.slider = new ui.ImageSlider();
-        root.slider.x = 60;
-        root.slider.width = 850;
-        root.slider.height = 850;
-        root.slider.configSlides(dir.liveResources.liveHeroBanners);
+        // root.slider = new ui.ImageSlider();
+        // root.slider.x = 60;
+        // root.slider.width = 850;
+        // root.slider.height = 850;
+        // root.slider.configSlides(dir.liveResources.liveHeroBanners);
+        root.holder = new we.ui.HorizontalHolder();
+        root.holder.x = 60;
+        root.holder.slideHeight = 850;
+        root.holder.slideWidth = 850;
+        root.holder.isAuto = true;
+        root.holder.isLoop = true;
+        root.holder.isBullet = true;
+        root.holder.height = 850;
+        root.holder.width = 850;
+        root.holder.bulletGapValue = 20;
+        root.holder.bulletBottom = 50;
+        root.holder.bulletHorizontalCenter = 0;
+        dir.liveResources.liveHeroBanners.forEach(element => {
+          const image = new eui.Image();
+          if (element.image) {
+            image.source = element.image;
+          }
+          root.holder.addChild(image);
+        });
 
         root.roomList.layout = root.roomListRefer.layout;
         root.roomList.itemRenderer = MobileLiveListHolder;
