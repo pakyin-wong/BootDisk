90b6b8d9b621c4d82935bb2f9f23aea4db1f1a72
angela <angela.dev@pgpg.la>
2020-08-27 14:55:46 +0800

    - fix WGFE-240 (style of mobilelivelistsimpleitem)

--- src/ui/common/ControlItem/mobile/MobileLiveListSimpleItem.ts
+++ src/ui/common/ControlItem/mobile/MobileLiveListSimpleItem.ts
@@ -129,7 +129,7 @@ namespace we {
 
       protected updateBetLimitText(items, idx) {
         if (this._toggler) {
-          this._toggler.renderText = () => ` ${items.length > 0 ? items[idx] : ''}`;
+          this._toggler.renderText = () => ` ${i18n.t('baccarat.betLimitshort')} ${items.length > 0 ? items[idx] : ''}`;
         }
       }
     }
