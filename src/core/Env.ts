769dc74a05d8ce12e23e87a398961b351e2e4164
joesh1203 <joesh1203@gmail.com>
2020-09-01 13:15:23 +0800

    v0.8.9

--- src/core/Env.ts
+++ src/core/Env.ts
@@ -16,7 +16,7 @@ namespace we {
       public UAInfo: any;
 
       /* Global Environment Variable */
-      public version: string = '0.8.8';
+      public version: string = '0.8.9';
       public versionNotShownIn = ['uat', 'production'];
       public initialized: boolean = false;
       public balance: number = NaN;
