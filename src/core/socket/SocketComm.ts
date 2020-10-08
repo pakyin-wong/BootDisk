7ba916808639b8256bd2fa5724dc1dc5039ff158
angelaTse <angela.dev@pgpg.la>
2020-10-08 13:59:33 +0800

    - fix LSB-90 (nickname display)

--- src/core/socket/SocketComm.ts
+++ src/core/socket/SocketComm.ts
@@ -283,7 +283,8 @@ namespace we {
         // env.icons = {};
 
         // env.nicknameKey = player.profile.nickname;
-        env.nickname = player.profile.settings.nickname;
+        // env.nickname = player.profile.settings.nickname;
+        env.nickname = player.profile.settings.nickname ? player.profile.settings.nickname : player.profile.nickname;
         // env.icons = {
         //   iconKey01: 'd_lobby_profile_pic_01_png',
         //   iconKey02: 'd_lobby_profile_pic_02_png',
