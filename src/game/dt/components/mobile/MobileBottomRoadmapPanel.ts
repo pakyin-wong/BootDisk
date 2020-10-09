f1555798096ca50ddc76d53e43c88b61d90e9b01
kenLCH <ken.dev@pgpg.la>
2020-10-09 12:24:47 +0800

    -fix dt mobile bottomroadmappanel wrong label and update total count label

--- src/game/dt/components/mobile/MobileBottomRoadmapPanel.ts
+++ src/game/dt/components/mobile/MobileBottomRoadmapPanel.ts
@@ -206,18 +206,19 @@ namespace we {
       public update() {
         if (this.tableInfo) {
           if (this.tableInfo.gamestatistic) {
+            console.log('dt :: this.tableInfo.gamestatistic',            console.log('dt :: this.tableInfo.gamestatistic',this.tableInfo.gamestatistic))
             if (this.tableInfo.gamestatistic.bankerCount) {
               this.bankerCountLabel.text = this.tableInfo.gamestatistic.bankerCount.toString();
             }
             if (this.tableInfo.gamestatistic.playerCount) {
               this.playerCountLabel.text = this.tableInfo.gamestatistic.playerCount.toString();
             }
-            // if (this.tableInfo.gamestatistic.tieCount) {
-            //   this.tieCountLabel.text = this.tableInfo.gamestatistic.tieCount.toString();
-            // }
-            // if (this.tableInfo.gamestatistic.bankerPairCount) {
-            //   this.bankerPairCountLabel.text = this.tableInfo.gamestatistic.bankerPairCount.toString();
-            // }
+            if (this.tableInfo.gamestatistic.tieCount) {
+              this.tieCountLabel.text = this.tableInfo.gamestatistic.tieCount.toString();
+            }
+            if (this.tableInfo.gamestatistic.bankerPairCount) {
+              this.bankerPairCountLabel.text = this.tableInfo.gamestatistic.bankerPairCount.toString();
+            }
             if (this.tableInfo.gamestatistic.playerPairCount) {
               this.playerPairCountLabel.text = this.tableInfo.gamestatistic.playerPairCount.toString();
             }
