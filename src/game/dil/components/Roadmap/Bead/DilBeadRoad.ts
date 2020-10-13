d3ff2e662c5a54e0d5bedfe7dd92c4e1f62dc13f
kenLCH <ken.dev@pgpg.la>
2020-10-13 14:35:20 +0800

    add eventlistener and dispatch event in dilbeadroad

--- src/game/dil/components/Roadmap/Bead/DilBeadRoad.ts
+++ src/game/dil/components/Roadmap/Bead/DilBeadRoad.ts
@@ -87,7 +87,6 @@ namespace we {
           this.expandRoad(this.isExpanded);
         }
       }
-
       public expandRoad(expand: boolean) {
         if (this.roadMapIconList && this.roadData) {
           // const min = Math.min(this.roadData.length, this.roadMapIconList.length);
@@ -103,6 +102,57 @@ namespace we {
           }
         }
         this.isExpanded = expand;
+      }
+      
+      private onMove(event: egret.TouchEvent) {
+        let pt: egret.Point = new egret.Point(0, 0);
+        this.globalToLocal(event.stageX, event.stageY, pt);
+        pt = pt;
+        const posX: number = pt.x;
+        const posY: number = pt.y;
+        if (posX > 0 && posX < this.gridSize * this.numCol && posY > 0 && posY < this.gridSize * 6) {
+          const col = Math.floor(posX / this.gridSize);
+          const row = Math.floor(posY / this.gridSize);
+          const index = col * 6 + row;
+          const iconValue = this.roadMapIconList[index].value;
+
+          if (iconValue.v !== undefined) {
+            // dispatch the result rolled over by the user
+            this.dispatchEvent(new egret.Event('RollOverResult', false, false, { index, mouseX: event.stageX, mouseY: event.stageY, gameRoundID: iconValue['gameRoundID'] }));
+          } else {
+            // dispatch rolled out result
+            this.dispatchEvent(new egret.Event('RollOutResult'));
+          }
+        } else {
+          // dispatch rolled out result
+          this.dispatchEvent(new egret.Event('RollOutResult'));
+        }
+      }
+      private onOut(event: mouse.MouseEvent) {
+        if (this.stage.hasEventListener(mouse.MouseEvent.MOUSE_MOVE)) {
+          mouse.setMouseMoveEnabled(false);
+          this.stage.removeEventListener(mouse.MouseEvent.MOUSE_MOVE, this.onMove, this);
+        }
+        // dispatch rolled out result
+        this.dispatchEvent(new egret.Event('RollOutResult'));
+      }
+      private onClick(event: egret.TouchEvent) {
+        let pt: egret.Point = new egret.Point(0, 0);
+        this.globalToLocal(event.stageX, event.stageY, pt);
+        pt = pt;
+        const posX: number = pt.x;
+        const posY: number = pt.y;
+        if (posX > 0 && posX < this.gridSize * this.numCol && posY > 0 && posY < this.gridSize * 6) {
+          const col = Math.floor(posX / this.gridSize);
+          const row = Math.floor(posY / this.gridSize);
+          const index = col * 6 + row;
+          const iconValue = this.roadMapIconList[index].value;
+
+          if (iconValue.v !== undefined) {
+            // dispatch the result click by the user
+            this.dispatchEvent(new egret.Event('ClickResult', false, false, { index, mouseX: event.stageX, mouseY: event.stageY, gameRoundID: iconValue['gameRoundID'] }));
+          }
+        }
       }
     }
   }
