f155766974d3f7002afa40120a510a7524633f36
joengsh <joengsh@gmail.com>
2020-10-13 13:46:19 +0800

    updated all desktop scene titleHeader

--- src/game/lo/LotterySceneFun.ts
+++ src/game/lo/LotterySceneFun.ts
@@ -19,6 +19,8 @@ namespace we {
       protected _lastgameResult;
       protected _drawerPanel: lo.LoRightDrawerPanel;
 
+      protected _titleHeader: eui.Group;
+
       protected mount() {
         super.mount();
         this.initDenom();
@@ -26,6 +28,29 @@ namespace we {
 
         if (this._drawerPanel) {
           this._drawerPanel.setTableInfo(this._tableInfo);
+        }
+      }
+
+      protected initOrientationDependentComponent() {
+        this._header && dir.layerCtr.nav && dir.layerCtr.nav.addChild(this._header);
+        console.log(this._titleHeader);
+        const titleGroup1: eui.Group = dir.layerCtr.nav.$children[0] as eui.Group;
+        const titleGroup: eui.Group = titleGroup1['_titleGroup'];
+        console.log(titleGroup);
+        this._titleHeader && titleGroup && titleGroup.addChild(this._titleHeader);
+        // this._header && this.sceneHeader.addChild(this._header);
+      }
+
+      protected clearOrientationDependentComponent() {
+        super.clearOrientationDependentComponent();
+        if (this._titleHeader && this._titleHeader.parent !== null) {
+          this._titleHeader.parent.removeChild(this._titleHeader);
+        }
+      }
+      protected destroy() {
+        super.destroy();
+        if (this._titleHeader && this._titleHeader.parent !== null) {
+          this._titleHeader.parent.removeChild(this._titleHeader);
         }
       }
 
