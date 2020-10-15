a99249653983545c9ec4c9c97fad746b3dbe4614
AngelaTse <angela.dev@pgpg.la>
2020-10-15 16:36:35 +0800

    - fix no.30 (RO statisticChart style)

--- src/game/ro/components/BarChart.ts
+++ src/game/ro/components/BarChart.ts
@@ -51,9 +51,9 @@ namespace we {
         x: number,
         y: number,
         height: number,
-        barRightColor: number[] = [0x3050e0, 0x607dff],
-        barMidColor: number[] = [0x188d43, 0x2dc85c],
-        barLeftColor: number[] = [0xff3c3c, 0xab2020]
+        barRightColor: number[] = [0x607dff, 0x3050e0], // blue
+        barMidColor: number[] = [0x2dc85c, 0x188d43], // green
+        barLeftColor: number[] = [0xff3c3c, 0xab2020] // red
       ) {
         this._totalWidth = totalWidth;
         this._a = a;
@@ -74,6 +74,7 @@ namespace we {
       }
 
       public draw() {
+
         const totalAmount = this._a + this._b + this._c;
         const widthA = this._totalWidth * (this._a / totalAmount);
         const widthB = this._totalWidth * (this._b / totalAmount);
@@ -82,35 +83,43 @@ namespace we {
         this._moveGroup.x = (this._x + widthA + widthB) / 2 + 27.5;
 
         const matrixGreen = new egret.Matrix();
-        matrixGreen.createGradientBox(widthB, this._height, 0, 0, 0);
+        matrixGreen.createGradientBox(widthB, this._height, Math.PI / 2, this._x + widthA, this._y);
 
         const rectGreen: egret.Shape = new egret.Shape();
-        rectGreen.graphics.beginGradientFill(egret.GradientType.RADIAL, this._barMidColor, [1, 1], [0, 255], matrixGreen);
+        rectGreen.graphics.beginGradientFill(egret.GradientType.LINEAR, this._barMidColor, [1, 1], [0, 255], matrixGreen);
         rectGreen.graphics.drawRect(this._x + widthA, this._y, widthB, this._height);
         rectGreen.graphics.endFill();
         this._staticGroup.addChild(rectGreen);
 
         const matrixRed = new egret.Matrix();
-        matrixRed.createGradientBox(widthA, this._height, 0, 0, 0);
+        matrixRed.createGradientBox(widthA, this._height, Math.PI / 2, this._x, this._y);
 
         const rectRed: egret.Shape = new egret.Shape();
-        rectRed.graphics.beginGradientFill(egret.GradientType.RADIAL, this._barLeftColor, [1, 1], [0, 255], matrixRed);
+        rectRed.graphics.beginGradientFill(egret.GradientType.LINEAR, this._barLeftColor, [1, 1], [0, 255], matrixRed);
         rectRed.graphics.drawRect(this._x, this._y, widthA, this._height);
         rectRed.graphics.endFill();
         this._staticGroup.addChild(rectRed);
 
         const matrixBlue = new egret.Matrix();
-        matrixBlue.createGradientBox(widthC, this._height, 0, 0, 0);
+        matrixBlue.createGradientBox(widthC, this._height, Math.PI / 2, this._x + this._totalWidth - widthC, this._y);
 
         const rectBlue: egret.Shape = new egret.Shape();
-        rectBlue.graphics.beginGradientFill(egret.GradientType.RADIAL, this._barRightColor, [1, 1], [0, 255], matrixBlue);
+        rectBlue.graphics.beginGradientFill(egret.GradientType.LINEAR, this._barRightColor, [1, 1], [0, 255], matrixBlue);
         rectBlue.graphics.drawRect(this._x + this._totalWidth - widthC, this._y, widthC, this._height);
         rectBlue.graphics.endFill();
         this._staticGroup.addChild(rectBlue);
 
+        const _barBorder = new ui.RoundRectShape();
+        _barBorder.width = this._totalWidth;
+        _barBorder.height = this._height;
+        _barBorder.setRoundRectStyle(this._totalWidth + 100, this._height + 100, { tl: 10, tr: 10, bl: 10, br: 10 }, '0x6d7278', 0, 3, 0x6d7278, 1);
+        _barBorder.x = this._x;
+        _barBorder.y = this._y;
+        this._staticGroup.addChild(_barBorder);
+
         const groupMask: egret.Shape = new egret.Shape();
         groupMask.graphics.beginFill(0x00000);
-        groupMask.graphics.drawRoundRect(this._x, this._y, this._totalWidth, this._height, 33, 33);
+        groupMask.graphics.drawRoundRect(this._x - 1, this._y - 1, this._totalWidth + 2, this._height + 2, 33, 33);
         groupMask.graphics.endFill();
         this._staticGroup.addChild(groupMask);
         this._staticGroup.mask = groupMask;
