1322f555bb792f865918811f096169fedb469f2b
joesh1203 <joesh1203@gmail.com>
2020-10-22 20:35:27 +0800

    updated portrait lobby layout

--- src/game/lobby/MPageContentInitializer.ts
+++ src/game/lobby/MPageContentInitializer.ts
@@ -2,57 +2,59 @@
 namespace we {
   export namespace lobby {
     export class MPageContentInitializer implements core.IContentInitializer {
+
+      protected _root: ILobbyPage;
+
       constructor() {}
 
       public initContent(root: ILobbyPage) {
+        this._root = root;
+
         // init image slider
-        root._bannerSlider.configSlides(dir.lobbyResources.homeLargeBanners);
+        this._root._bannerSlider.configSlides(dir.lobbyResources.homeLargeBanners);
 
         if (root._bannerSlider && root._sliderBullet) {
           root._bannerSlider.bullets = root._sliderBullet;
           root._sliderBullet.imageSlider = root._bannerSlider;
         }
-        // init 4 featured posters
-        for (let i = 0, len = dir.lobbyResources.homeLargeBanners.length; i < len; i += 1) {
-          const { image, link } = dir.lobbyResources.homeLargeBanners[i];
-          const poster = new eui.Image();
-          poster['link'] = link;
-          poster.source = image;
-          poster.addEventListener(
-            egret.TouchEvent.TOUCH_TAP,
-            () => {
-              logger.l(utils.LogTarget.DEBUG, 'psoter click', i, link);
-              if (i === 0) {
-                dir.sceneCtr.goto('lobby', { page: 'live', tab: 'ba' });
-              } else if (i === 3) {
-                dir.sceneCtr.goto('lobby', { page: 'live', tab: 'di' });
-              } else {
-                dir.sceneCtr.goto('lobby', { page: 'live', tab: 'ro' });
-              }
-            },
-            this
-          );
-          // poster.addEventListener(egret.TouchEvent.TOUCH_TAP, this.bannerClicked, this);
-          root._posterContainer.addChild(poster);
-        }
+
+        this.reloadBanners();
 
         root._txt_hotgame.renderText = () => i18n.t('mobile_lobby_feature_title');
-
-        // init 3 grids
-        dir.lobbyResources.homeBanners.forEach(banner => {
-          const image = new eui.Image();
-          image.source = banner.image;
-          image['link'] = banner.link;
-          image.addEventListener(egret.TouchEvent.TOUCH_TAP, this.bannerClicked, this);
-          root._hotgameContainer.addChild(image);
-        });
       }
 
       protected bannerClicked(e: egret.Event) {
-        logger.l(utils.LogTarget.DEBUG, 'psoter click', e.currentTarget['link']);
+        logger.l(utils.LogTarget.DEBUG, 'poster click', e.currentTarget['link']);
       }
 
-      public reloadBanners() {}
+      public reloadBanners() {
+        this._root._posterContainer.removeChildren();
+        this._root._hotgameContainer.removeChildren();
+
+        for (let i = 0, len = Math.min(dir.lobbyResources.homeLargeBanners.length, 4); i < len; i++) {
+          const { image, link } = dir.lobbyResources.homeLargeBanners[i];
+          const poster = new LobbyBannerItem();
+          poster.skinName = 'skin_mobile.LargeBannerSkin';
+          poster.texture = image;
+          poster.link = link;
+          poster.hoverScale = 1;
+          this._root._posterContainer.addChild(poster);
+        }
+
+        // init 3 grids
+        dir.lobbyResources.homeBanners.forEach(banner => {
+          const { image, link, title, description } = banner;
+          const poster = new LobbyBannerItem();
+          poster.skinName = 'skin_mobile.SmallBannerSkin';
+          poster.texture = image;
+          poster.link = link;
+          poster.title = title;
+          poster.description = description;
+          poster.hoverScale = 1;
+          this._root._hotgameContainer.addChild(poster);
+        });
+
+      }
     }
   }
 }
