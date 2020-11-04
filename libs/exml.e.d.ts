ddbd6c6c96a535178caee60329a70690059dc895
AngelaTse <angela.dev@pgpg.la>
2020-11-04 13:19:31 +0800

    ( in progress) update betRelatedGroup in desktop

--- libs/exml.e.d.ts
+++ libs/exml.e.d.ts
@@ -221,6 +221,10 @@ declare module skin_desktop{
 	}
 }
 declare module skin_desktop{
+	class BetRelatedGroup extends eui.Skin{
+	}
+}
+declare module skin_desktop{
 	class LiveListAdvancedItemSkin extends eui.Skin{
 	}
 }
