c400ceeeb9cf842bcef8407513a3f08312c3affe
Joe Ng <joengsh@gmail.com>
2020-11-16 19:02:55 +0800

    added description of BetRelatedAnimationButton

--- src/ui/generic/BetRelatedAnimationButton.ts
+++ src/ui/generic/BetRelatedAnimationButton.ts
@@ -3,9 +3,14 @@ namespace we {
   export namespace ui {
     export class BetRelatedAnimationButton extends we.core.BaseEUI {
       /**
-       * requirement:
-       * support state: idle, hovered, pressed, disabled
-       * 
+       * requirement: (may consider modify or override BaseAnimationButton)
+       * support button state: idle, hovered, pressed, disabled
+       * support animation (state): idle, hover, release, disable
+       * support transition animation: [state_A]_to_[state_B] (e.g. idle_to_hover, hover_to_idle, hover_to_release, release_to_hover, ...)
+       * logic: each time when changing state, i.e. from A to B, check if animation with name "[state_A]_to_[state_B]" exists, if yes, play it, else play "[state_B]"
+       *        when playing "release", no matter the button changed to hovered/ idle/ disabled, it needs to wait for "release" animation completed before playing another animation
+       *        (e.g. player "release" -> state to hovered -> state to idle -> "release" completed -> check if "release_to_idle" exist, else play "idle")
+       *        when playing "release", user press the button again (i.e. followed by another "release" animation), the new "release" animation will be played immediately
        **/
     }
   }
