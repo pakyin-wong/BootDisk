255d3a78deaa9e0465c749f5ec8c491f89e7aec5
Joe Ng <joengsh@gmail.com>
2020-11-16 18:26:50 +0800

    added BetRelatedAnimationButton

---
+++ src/ui/generic/BetRelatedAnimationButton.ts
@@ -0,0 +1,12 @@
+// TypeScript file
+namespace we {
+  export namespace ui {
+    export class BetRelatedAnimationButton extends we.core.BaseEUI {
+      /**
+       * requirement:
+       * support state: idle, hovered, pressed, disabled
+       * 
+       **/
+    }
+  }
+}
