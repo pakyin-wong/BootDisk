9b4839ca1b6a682adfeed22922a30e5bceb760e8
AngelaTse <angela.dev@pgpg.la>
2020-11-17 19:56:10 +0800

    (in progress)

--- src/ui/generic/BetRelatedAnimationButton.ts
+++ src/ui/generic/BetRelatedAnimationButton.ts
@@ -1,7 +1,7 @@
 // TypeScript file
 namespace we {
   export namespace ui {
-    export class BetRelatedAnimationButton extends we.core.BaseEUI {
+    export class BetRelatedAnimationButton extends BaseAnimationButton implements IButton {
       /**
        * requirement: (may consider modify or override BaseAnimationButton)
        * support button state: idle, hovered, pressed, disabled
@@ -15,6 +15,63 @@ namespace we {
       public constructor() {
         super();
         this.orientationDependent = false;
+      }
+
+      protected mount() {
+        super.mount();
+        switch (this._dbDisplay) {
+          default:
+            this.active = true;
+            this.isSwitch = true;
+            this.isSetting = false;
+            this.checkEnable();
+            break;
+        }
+
+        this.addEventListeners();
+      }
+
+      public destroy() {
+        super.destroy();
+        this.removeEventListeners();
+      }
+
+      protected addEventListeners() {
+      }
+
+      protected removeEventListeners() { 
+      }
+
+      protected onRollover() {
+        super.onRollover();
+        console.log('to be relase');
+        this.playPromise('release', 5);
+      }
+
+      protected onRollout() {
+        super.onRollout();
+        console.log('rollOut');
+        const oldState = [this._down, this._hover];
+        this._down = false;
+        this._hover = false;
+        this.update(oldState);
+      }
+
+      protected checkEnable() {
+        this.playPromise('release', 5);
+      }
+      public onIdle() {
+        this.playPromise('idle', 5);
+      }
+      public onHover() {
+        this.playPromise('hover', 5);
+      }
+      public onRelease() {
+        console.log('.......release');
+        this.playPromise('release', 5);
+      }
+      public onDisable() {
+        this.playPromise('disable', 5);
       }
 
     }
