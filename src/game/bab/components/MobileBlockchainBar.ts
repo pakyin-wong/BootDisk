9e17d46dcfc4acbf77ece8d3ed2d4dc4a1791666
jackydevpgpg <jacky.dev@pgpg.la>
2020-11-23 14:04:35 +0800

    update mobile blockchain dragonbones bar

---
+++ src/game/bab/components/MobileBlockchainBar.ts
@@ -0,0 +1,210 @@
+// TypeScript file
+namespace we {
+  export namespace blockchain {
+    export class MobileBlockchainBar extends core.BaseEUI {
+
+      protected _anim : dragonBones.EgretArmatureDisplay;
+      
+      protected factor = 100;
+
+      protected _redBarLimit ;
+      protected _blueBarLimit;
+      protected _blueBarLimitX ;
+      protected _redBarLimitX ;
+      protected _centerControl :dragonBones.Bone;
+      protected _redBarEffect;
+      protected _blueBarEffect;
+
+      protected _barOffsetX = 555;
+      protected _effectCenter = 553;
+      protected _centerOriginX = 0;
+
+
+      protected _lblRed : ui.RunTimeLabel;
+      protected _lblBlue : ui.RunTimeLabel;
+
+      protected _blueTargetNumber : number;
+      protected _redTargetNumber : number;
+
+      protected _isUpdating : boolean = false;
+
+      protected currentProgress : number = 0;
+      protected _barGrp : eui.Group;
+      
+      constructor(blue : number, red : number){
+        super();
+
+        this._blueTargetNumber = blue;
+        this._redTargetNumber = red;
+
+        this.initSkin();
+      }
+
+      protected initSkin(){
+        if(env.orientation === 'portrait'){
+          this.skinName = 'skin_mobile_portrait.MobileBlockchainBar';
+        }else{
+          this.skinName = 'skin_mobile_landscape.MobileBlockchainBar';
+        }
+      }
+
+      protected mount(){
+        super.mount();
+      }
+
+      protected initComponents(){
+        super.initComponents();
+
+        const skeletonData = RES.getRes(`blockchain_ske_json`);
+        const textureData = RES.getRes(`blockchain_tex_json`);
+        const texture = RES.getRes(`blockchain_tex_png`);
+        const factory: dragonBones.EgretFactory = new dragonBones.EgretFactory();
+        factory.parseDragonBonesData(skeletonData);
+        factory.parseTextureAtlasData(textureData, texture);
+        // const chip = factory.buildArmatureDisplay('poker');
+        this._anim = factory.buildArmatureDisplay('bet_bar');
+
+        // this.anim.x = this.stage.width/2;
+        // this.anim.y = this.stage.height/2;
+
+        this._barGrp.addChild(this._anim);
+        this._anim.x = this._anim.width/2;
+        this._anim.y = -this._anim.height/2;
+        this._anim.anchorOffsetX = 0;
+        this._anim.anchorOffsetY = 0;
+
+        const bar = this._anim.armature.getBone('bar_group');
+        this._centerControl = bar.armature.getBone('bar_center_control');
+
+        this._redBarLimit = bar.armature.getBone('red_bar_limit');
+        this._blueBarLimit = bar.armature.getBone('blue_bar_limit');
+
+        this._redBarEffect = this._redBarLimit.armature.getBone('red_effect_position');
+        this._blueBarEffect = this._blueBarLimit.armature.getBone('blue_effect_position');
+        
+        this._blueBarLimitX = this._redBarEffect.origin.x;
+        this._redBarLimitX = this._blueBarEffect.origin.x;
+
+        this._centerControl.origin.x = 0;
+        this._centerOriginX = this._centerControl.origin.x;
+
+        dir.meterCtr.register('blockchainlblblue',this._lblBlue);
+        dir.meterCtr.register('blockchainlblred',this._lblRed);
+
+        this.initBarProgress();
+      }
+
+      protected initBarProgress(){
+        // if(env.orientation === 'portrait'){
+        //   this._barOffsetX = 555;
+        //   this._effectCenter = 553;
+        // }else{
+
+        // }
+        this._barOffsetX = 555;
+        this._effectCenter = 553;
+
+        this.currentProgress = (this._blueTargetNumber - this._redTargetNumber ) / (this._redTargetNumber + this._blueTargetNumber);
+        this._centerControl.origin.x = this._centerOriginX + this.currentProgress * this._barOffsetX;
+
+        this._lblBlue.renderText = () => `${utils.formatNumber(this._blueTargetNumber, true)}`;
+        this._lblRed.renderText = () => `${utils.formatNumber(this._redTargetNumber, true)}`;
+      }
+
+      public playAnim(red : number, blue : number){
+        this.updateBarInfo(red, blue);
+      }
+
+      protected moveEffect(r, b){
+
+        egret.Tween.removeTweens(this._centerControl.origin);
+        egret.Tween.removeTweens(this._redBarEffect.origin);
+        egret.Tween.removeTweens(this._blueBarEffect.origin);
+
+        this._blueTargetNumber = b;
+        this._redTargetNumber = r;
+
+        dir.meterCtr.rackTo('blockchainlblblue',this._blueTargetNumber,700);
+        dir.meterCtr.rackTo('blockchainlblred',this._redTargetNumber,700);
+
+        this.currentProgress = (this._blueTargetNumber - this._redTargetNumber ) / (this._redTargetNumber + this._blueTargetNumber);
+        const des = this._centerOriginX + this.currentProgress * this._barOffsetX;;
+
+        egret.Tween.get(this._centerControl.origin)
+          .to({ x: des }, 230, t => {
+            this._centerControl.invalidUpdate();
+            return t;
+        });
+
+        egret.Tween.get(this._blueBarEffect.origin)
+          .to({ x: (this._effectCenter + des)}, 230, t => {
+            this._blueBarEffect.invalidUpdate();
+            return t;
+        });
+
+        egret.Tween.get(this._redBarEffect.origin)
+          .to({ x: (-this._effectCenter + des)}, 230, t => {
+            this._redBarEffect.invalidUpdate();
+            return t;
+        });
+      }
+
+      protected updateBarInfo(red : number, blue : number){
+
+        this.moveEffect(red, blue);
+
+        if(this._isUpdating){
+          return;
+        }
+
+        this._isUpdating = true;
+        // const barEffect = des > 0 ? this._blueBarEffect : this._redBarEffect;
+        // const effectOffset = des > 0 ? 1 : -1; 
+        // const effectDes = this._effectCenter  * effectOffset + des;
+        // const barAnim = des > 0 ? 'effect_blue' : 'effect_red';
+
+        this._anim.once(dragonBones.AnimationEvent.COMPLETE,this.onAnimCompleted,this);
+
+        // this._anim.animation.play(barAnim,1);
+        this._anim.animation.fadeIn('effect_red',0,1,0,'redeffectgroup');
+        this._anim.animation.fadeIn('effect_blue',0,1,0,'blueeffectgroup');
+      }
+
+      protected async onAnimCompleted(e = null){
+        const anim : dragonBones.EgretArmatureDisplay = e.target;
+        await utils.sleep(1);
+        anim.animation.reset();
+        anim.once(dragonBones.AnimationEvent.COMPLETE,this.resetAnim,this);
+        anim.animation.play('effect_center',1);
+      }
+
+      protected resetAnim(e:dragonBones.AnimationEvent = null){
+        const anim : dragonBones.EgretArmatureDisplay = e.target;
+        anim.animation.reset();
+
+        this._lblBlue.renderText = () =>`${this._blueTargetNumber}`;
+        this._lblRed.renderText = () =>`${this._redTargetNumber}`;
+
+        this._blueBarEffect.origin.x = this._blueBarLimitX;
+        this._redBarEffect.origin.x = this._redBarLimitX;
+
+        this._isUpdating = false;
+
+        const a = 1000;
+        const red = 500 - this.factor;
+
+        this.playAnim(red,1000);
+        this.factor *= -1;
+      }
+
+      protected destroy(){
+        this.resetAnim();
+        super.destroy();
+
+        dir.meterCtr.drop('blockchainlblblue',this._lblBlue);
+        dir.meterCtr.drop('blockchainlblred',this._lblRed);      
+      }
+
+    }
+  }
+}
