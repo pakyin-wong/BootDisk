830f2cdc670aecf18161a555d1030679e7c10d8a
Joe Ng <joengsh@gmail.com>
2020-11-27 18:09:21 +0800

    updated CardInfoPanel ThirdPartyButton handling

--- src/ui/common/Blockchain/CardInfoPanel.ts
+++ src/ui/common/Blockchain/CardInfoPanel.ts
@@ -91,11 +91,11 @@ namespace we {
         this._thirdPartyButton.addEventListener(
           egret.TouchEvent.TOUCH_TAP,
           () => {
-            // window.open(env.blockchain.thirdPartySHA256);
-            utils.copyToClipboard(env.blockchain.thirdPartySHA256);
-            if (this._message) {
-              this._message.showMessage(ui.InGameMessage.INFO, i18n.t('message.urlcopied'));
-            }
+            window.open(env.blockchain.thirdPartySHA256,'_blank', 'toolbar=0,location=0,menubar=0');
+            // utils.copyToClipboard(env.blockchain.thirdPartySHA256);
+            // if (this._message) {
+            //   this._message.showMessage(ui.InGameMessage.INFO, i18n.t('message.urlcopied'));
+            // }
           },
           this
         );
