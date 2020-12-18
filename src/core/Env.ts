ce73dea0d4fa0aea561cde122c8c1fa8f5d586d1
Joe Ng <joengsh@gmail.com>
2020-12-19 00:17:04 +0800

    v2.0.8

--- src/core/Env.ts
+++ src/core/Env.ts
@@ -20,7 +20,7 @@ namespace we {
 
       /* Global Environment Variable */
       public mainVersion: string = '2.0';
-      protected patch: string = '7';
+      protected patch: string = '8';
 
       public get version(): string {
         return `${this.mainVersion}.${this.patch}`;
